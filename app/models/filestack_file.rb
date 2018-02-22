3a4d812b5b394e4257aeac4bed515cf539e856f3
Josh Schwartzman <josh@dreamseedo.org>
2018-02-21 16:38:17 -0800

    Fix test

--- app/models/filestack_file.rb
+++ app/models/filestack_file.rb
@@ -3,11 +3,22 @@ class FilestackFile < ApplicationRecord
 
   validates :url, :handle, presence: true
 
+  after_create :process_image, if: :image?
   after_destroy :delete_on_filestack
+
+  def image?
+    mimetype.include?('image')
+  end
 
   private
 
+  def process_image
+    # TODO: We will want do decide what kind of post-processing to do on uploaded files
+    # docs: https://www.filestack.com/docs/image-transformations
+  end
+
   def delete_on_filestack
-    # TODO: write method
+    # TODO: write this method
+    # docs: https://www.filestack.com/docs/rest-api/remove
   end
 end
