bcca146d192191a059d6233082c050cd252a4f00
Marco Segreto <msegreto@ideo.com>
2018-03-16 18:34:22 -0700

    Pill style like mockup

--- app/javascript/ui/global/PillList.js
+++ app/javascript/ui/global/PillList.js
@@ -5,12 +5,31 @@ import styled from 'styled-components'
 import { withStyles } from 'material-ui/styles'
 import Chip from 'material-ui/Chip'
 
+import v from '~/utils/variables'
+import CloseIcon from '~/ui/icons/CloseIcon'
 import UserAvatar from '~/ui/layout/UserAvatar'
 
 const materialStyles = {
   chip: {
+    padding: '7px',
     margin: '5px',
+    fontWeight: 300,
+    fontFamily: 'Gotham',
+    fontSize: '1rem',
+    justifyContent: 'flex-start',
+    backgroundColor: v.colors.desert,
+    borderRadius: 0,
   },
+  avatar: {
+    height: '38px',
+    width: '38px',
+  },
+  icon: {
+    width: '9px',
+    height: '20px',
+    marginLeft: '0px',
+    marginBottom: '2px',
+  }
 }
 
 const ChipHolder = styled.div`
@@ -34,7 +53,7 @@ class PillList extends React.Component {
 
           if (item.pic_url_square) {
             // TODO want to use a generic avatar here
-            avatar = <UserAvatar user={item} />
+            avatar = <UserAvatar size={38} user={item} />
           }
 
           return (
@@ -44,6 +63,7 @@ class PillList extends React.Component {
               label={item.name}
               onDelete={this.handleDelete(item)}
               className={classes.chip}
+              deleteIcon={<span className={classes.icon}><CloseIcon /></span>}
             />
           )
         })}
