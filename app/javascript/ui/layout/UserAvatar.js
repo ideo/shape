84b7446de2758f6b1beb0eebaa2a2761e53eabec
Marco Segreto <msegreto@ideo.com>
2018-03-19 17:22:02 -0700

    Add a generic avatar to use

---
+++ app/javascript/ui/layout/UserAvatar.js
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types'
+import { observer, PropTypes as MobxPropTypes } from 'mobx-react'
+
+import Avatar from '~/ui/global/Avatar'
+
+@observer
+class UserAvatar extends React.Component {
+  render() {
+    const { user, className, size } = this.props
+    return (
+      <Avatar
+        title={user.name}
+        size={size}
+        className={className}
+        url={user.pic_url_square}
+      />
+    )
+  }
+}
+
+UserAvatar.propTypes = {
+  user: MobxPropTypes.objectOrObservableObject.isRequired,
+  size: PropTypes.number,
+  className: PropTypes.string,
+}
+
+UserAvatar.defaultProps = {
+  size: 34,
+  className: 'avatar',
+}
+
+export default UserAvatar
