dc4a5003772c1677960b182d66b3b1e84dfde718
Marco Segreto <msegreto@ideo.com>
2018-03-20 14:36:19 -0700

    Get groups modification mostly working
    The roles aren't setup correctly so you can't see the role
    but besides that it all works

--- app/javascript/ui/groups/GroupModify.js
+++ app/javascript/ui/groups/GroupModify.js
@@ -1,96 +1,16 @@
 import PropTypes from 'prop-types'
-import { action, observable } from 'mobx'
+import { action, observable, toJS } from 'mobx'
 import { observer, PropTypes as MobxPropTypes } from 'mobx-react'
-import styled from 'styled-components'
+import {
+  FormButton,
+  FieldContainer,
+  FormActionsContainer,
+  Label,
+  ImageField,
+  TextField,
+} from '~/ui/global/styled/forms'
 import FilestackUpload from '~/utils/FilestackUpload'
-import v from '~/utils/variables'
 import Group from '~/stores/jsonApi/Group'
-
-// TODO remove duplication with GridCardBlank
-const BctButton = styled.button`
-`
-BctButton.displayName = 'BctButton'
-
-const StyledLabel = styled.div`
-  text-transform: uppercase;
-  margin-bottom: 20px;
-  font-family: Gotham;
-  font-size: 16px;
-  font-weight: 500;
-  letter-spacing: 1px;
-  display: block;
-`
-
-StyledLabel.displayName = 'StyledLabel'
-
-const StyledFieldBox = styled.div`
-  padding-bottom: 35px;
-
-  label {
-    margin-right: 15px;
-  }
-`
-StyledFieldBox.displayName = 'StyledFieldBox'
-
-const StyledActionBox = styled.div`
-  padding-bottom: 14px;
-  text-align: center;
-`
-
-const StyledTextbox = styled.input`
-  width: 224px;
-  padding-right: 4px;
-  padding-left: 4px;
-  padding-bottom: 6px;
-  outline-width: 0;
-  font-size: 16px;
-  border: 0;
-  border-bottom: 0.5px solid ${v.colors.gray};
-
-  &::placeholder {
-    color: ${v.colors.gray};
-  }
-
-  &:focus {
-    outline-width: 0;
-  }
-`
-StyledTextbox.displayName = 'StyledTextbox'
-
-const StyledSubmit = styled.input`
-  width: 183px;
-  height: 40px;
-  border-radius: 19.5px;
-  border: none;
-  background-color: ${v.colors.blackLava};
-  text-transform: uppercase;
-  font-family: Gotham;
-  font-size: 16px;
-  font-weight: 500;
-  letter-spacing: 1.5px;
-  cursor: pointer;
-  color: #ffffff;
-`
-StyledSubmit.displayName = 'StyledSubmit'
-
-const StyledAddImageIcon = styled.span`
-  width: 100px;
-  position: relative;
-  height: 100px;
-  display: block;
-  color: #ffffff;
-  border-radius: 50%;
-  background-color: ${v.colors.gray};
-
-  span {
-    position: absolute;
-    font-size: 36px;
-    font-weight: 300;
-    left: calc(50% - 8px);
-    top: calc(50% - 23px);
-  }
-`
-StyledAddImageIcon.displayName = 'StyledAddImageIcon'
 
 @observer
 class GroupModify extends React.Component {
@@ -99,8 +19,8 @@ class GroupModify extends React.Component {
     const { group } = props
     this.editingGroup = {
       name: group.name || '',
-      tag: group.tag || '',
-      pic_url_square: group.pic_url_square || '',
+      handle: group.handle || '',
+      filestack_file_url: group.filestack_file_url || '',
     }
   }
 
@@ -112,21 +32,21 @@ class GroupModify extends React.Component {
   }
 
   @action
-  changeTag(tag) {
-    this.editingGroup.tag = tag
+  changeHandle(handle) {
+    this.editingGroup.handle = handle
   }
 
   @action
   changeUrl(url) {
-    this.editingGroup.pic_url_square = url
+    this.editingGroup.filestack_file_url = url
   }
 
   handleNameChange = (ev) => {
     this.changeName(ev.target.value)
   }
 
-  handleTagChange = (ev) => {
-    this.changeName(ev.target.value)
+  handleHandleChange = (ev) => {
+    this.changeHandle(ev.target.value)
   }
 
   handleImagePick = (ev) => {
@@ -146,11 +66,14 @@ class GroupModify extends React.Component {
     ev.preventDefault()
     const { onSave } = this.props
     let { group } = this.props
-    if (!group.id) group = new Group()
-    group.name = this.editingGroup.name
-    group.tag = this.editingGroup.tag
-    group.pic_url_square = this.editingGroup.pic_url_square
-    group.save().then(() => {
+    if (!group.id) {
+      group = new Group(toJS(this.editingGroup))
+    } else {
+      group.name = this.editingGroup.name
+      group.handle = this.editingGroup.handle
+      group.filestack_file_url = this.editingGroup.filestack_file_url
+    }
+    group.save().then((res) => {
       onSave && onSave()
     })
   }
@@ -158,43 +81,44 @@ class GroupModify extends React.Component {
   render() {
     return (
       <form>
-        <StyledFieldBox>
-          <StyledLabel htmlFor="groupName">Group Name</StyledLabel>
-          <StyledTextbox
+        <FieldContainer>
+          <Label htmlFor="groupName">Group Name</Label>
+          <TextField
             id="groupName"
             type="text"
             value={this.editingGroup.name}
             onChange={this.handleNameChange}
             placeholder="Enter Group Name"
           />
-        </StyledFieldBox>
-        <StyledFieldBox>
-          <StyledLabel htmlFor="groupTag">Group Tag</StyledLabel>
-          <StyledTextbox
-            id="groupTag"
+        </FieldContainer>
+        <FieldContainer>
+          <Label htmlFor="grouphandle">Group handle</Label>
+          <TextField
+            id="grouphandle"
             type="text"
-            value={this.editingGroup.tag}
-            onChange={this.handleTagChange}
-            placeholder="@group-tag"
+            value={this.editingGroup.handle}
+            onChange={this.handleHandleChange}
+            placeholder="@group-handle"
           />
-        </StyledFieldBox>
-        <StyledFieldBox>
-          <StyledLabel htmlFor="groupAvatar">Group Avatar</StyledLabel>
-          <BctButton onClick={this.handleImagePick} id="groupAvatar">
-            <StyledAddImageIcon>
+        </FieldContainer>
+        <FieldContainer>
+          <Label htmlFor="groupAvatar">Group Avatar</Label>
+          <button onClick={this.handleImagePick} id="groupAvatar">
+            <ImageField>
               <span>
                 +
               </span>
-            </StyledAddImageIcon>
-          </BctButton>
-        </StyledFieldBox>
-        <StyledActionBox>
-          <StyledSubmit
+            </ImageField>
+          </button>
+        </FieldContainer>
+        <FormActionsContainer>
+          <FormButton
             onClick={this.handleSave}
             type="submit"
-            value="save"
-          />
-        </StyledActionBox>
+          >
+            Save
+          </FormButton>
+        </FormActionsContainer>
       </form>
     )
   }
