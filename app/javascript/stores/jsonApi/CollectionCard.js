f8b9e149916a5b0e43b367b3ea882c71c929e759
kaptron <dkaplan@ideo.com>
2018-03-23 14:09:18 -0700

    Front-end fix for archiving last card in the collection

--- app/javascript/stores/jsonApi/CollectionCard.js
+++ app/javascript/stores/jsonApi/CollectionCard.js
@@ -15,9 +15,15 @@ class CollectionCard extends BaseRecord {
     // eslint-disable-next-line no-alert
     const agree = window.confirm('Are you sure?')
     if (agree) {
+      const collection = this.parent
+      let lastCard = false
       return this.apiStore.request(`collection_cards/${this.id}/archive`, 'PATCH')
         .then((response) => {
-          this.apiStore.fetch('collections', this.parent.id, true)
+          if (collection.collection_cards.length === 1) lastCard = true
+          this.apiStore.fetch('collections', collection.id, true).then(() => {
+            // for some reason it doesn't remove the last card when you re-fetch
+            if (lastCard) collection.emptyCards()
+          })
         })
         .catch((error) => {
           console.warn(error)
