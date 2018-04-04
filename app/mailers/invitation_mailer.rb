8add785f4597914f7a1b68eeb22ab5943cf517a1
kaptron <dkaplan@ideo.com>
2018-04-04 13:42:41 -0700

    Simpilified InvitationMailer setup

--- app/mailers/invitation_mailer.rb
+++ app/mailers/invitation_mailer.rb
@@ -1,14 +1,11 @@
 class InvitationMailer < ApplicationMailer
-  def invite(user_id:, invited_by_id:, invited_to_id: nil, invited_to_type: nil)
+  def invite(user_id:, invited_by_id:, invited_to_id:, invited_to_type:)
     @user = User.find(user_id)
     @invited_by = User.find(invited_by_id)
-    if invited_to_type && invited_to_id
-      @invited_to_type = invited_to_type
-      @invited_to = invited_to_type.safe_constantize.find(invited_to_id)
-      invited_to_url = frontend_url_for(@invited_to)
-    else
-      invited_to_url = root_url
-    end
+    @invited_to_type = invited_to_type
+    @invited_to = invited_to_type.safe_constantize.find(invited_to_id)
+    invited_to_url = frontend_url_for(@invited_to)
+
     if @user.pending?
       @url = accept_invitation_url(token: @user.invitation_token, redirect: invited_to_url)
     else
