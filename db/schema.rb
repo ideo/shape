3cf416c1a16babe3aa3f01c7dfb6205aa1dd3234
Marco Segreto <msegreto@ideo.com>
2018-04-04 18:00:37 -0700

    Add created_by to collections

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20180406184838) do
+=======
+ActiveRecord::Schema.define(version: 20180405001539) do
+>>>>>>> Add created_by to collections
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
