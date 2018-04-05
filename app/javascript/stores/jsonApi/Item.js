5d4a74c0881ca1636476f2d58fd7b6ed5fc3f88d
kaptron <dkaplan@ideo.com>
2018-04-05 15:24:37 -0700

    Adds whitelist of attributes for JS to send when doing a save

--- app/javascript/stores/jsonApi/Item.js
+++ app/javascript/stores/jsonApi/Item.js
@@ -3,6 +3,18 @@ import { archive } from './shared'
 import BaseRecord from './BaseRecord'
 
 class Item extends BaseRecord {
+  attributesForAPI = [
+    'type',
+    'name',
+    'content',
+    'text_data',
+    'url',
+    'image',
+    'archived',
+    'tag_list',
+    'filestack_file_attributes'
+  ]
+
   get parentPath() {
     if (this.breadcrumb && this.breadcrumb.length > 1) {
       const [type, id] = this.breadcrumb[this.breadcrumb.length - 2]
