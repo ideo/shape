814adbcf05f71b87b4b59990d7d09b9c6126ee27
Marco Segreto <msegreto@ideo.com>
2018-04-09 13:51:31 -0700

    Some cleanup

--- app/controllers/api/v1/collections_controller.rb
+++ app/controllers/api/v1/collections_controller.rb
@@ -17,7 +17,6 @@ class Api::V1::CollectionsController < Api::V1::BaseController
   end
 
   def create
-    debugger
     builder = CollectionBuilder.new(params: collection_params,
                                     organization: @organization,
                                     parent_card: @collection_card,
--- db/migrate/20180405001539_rename_created_by_in_collection.rb
+++
@@ -1,4 +0,0 @@
-class RenameCreatedByInCollection < ActiveRecord::Migration[5.1]
-  def change
-  end
-end
