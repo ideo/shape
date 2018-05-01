6f02e64069e6fe30acdc3045d9aa5dd2d1633a80
kaptron <dkaplan@ideo.com>
2018-04-30 18:04:37 -0700

    Spec fixes

--- spec/models/collection/shared_with_me_collection_spec.rb
+++ spec/models/collection/shared_with_me_collection_spec.rb
@@ -2,40 +2,46 @@ require 'rails_helper'
 
 describe Collection::SharedWithMeCollection, type: :model do
   describe '.find_or_create_for_collection' do
+    # creating the org will auto create SharedWithMeCollections for primary/guest groups
+    let!(:organization) { create(:organization) }
     let(:user) { create(:user) }
-    let(:parent_collection) { create(:collection) }
+    let(:parent_collection) { create(:collection, organization: organization) }
     let(:shared_with_me_collection) {
       Collection::SharedWithMeCollection.find_or_create_for_collection(
         parent_collection,
-        user
+        user,
       )
     }
 
-    it 'should create a Collection::UserCollection' do
+    it 'should create a Collection::SharedWithMeCollection' do
       expect {
         shared_with_me_collection
-      }.to change(Collection::SharedWithMeCollection, :count).by(2)
+      }.to change(Collection::SharedWithMeCollection, :count).by(1)
     end
 
-    it 'the collection should belong to same org as parent' do
+    it 'should belong to same org as parent' do
       expect(shared_with_me_collection.organization).to eq(parent_collection.organization)
     end
 
-    it 'should add user as editor' do
+    it 'should add user as viewer' do
       expect(user.has_role?(Role::VIEWER, shared_with_me_collection.becomes(Collection))).to be true
     end
   end
 
   describe '.create_for_group' do
-    let(:organization) { create(:organization) }
+    let!(:organization) { create(:organization) }
     let(:shared_with_me_collection) {
       Collection::SharedWithMeCollection.create_for_group(organization)
     }
 
-    it 'should create a Collection::UserCollection' do
+    it 'should create a Collection::SharedWithMeCollection' do
       expect {
         shared_with_me_collection
-      }.to change(Collection::SharedWithMeCollection, :count).by(2)
+      }.to change(Collection::SharedWithMeCollection, :count).by(1)
+    end
+
+    it 'should belong to the indicated org' do
+      expect(shared_with_me_collection.organization).to eq(organization)
     end
   end
 end
