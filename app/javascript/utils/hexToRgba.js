f06658d25420d36c790bb3bb09c2a65d353b90ff
Marco Segreto <msegreto@ideo.com>
2018-05-03 16:31:52 -0700

    Fix hexrgba file

--- app/javascript/utils/hexToRgba.js
+++ app/javascript/utils/hexToRgba.js
@@ -1,41 +1,50 @@
-const hexChars = 'a-f\\d';
-const match3or4Hex = `#?[${hexChars}]{3}[${hexChars}]?`;
-const match6or8Hex = `#?[${hexChars}]{6}([${hexChars}]{2})?`;
+/*
+ * The following code was taken from the hex-rgb library.
+ * It's here because it doesn't transpile it's template strings, therefore
+ * will break in Internet Explorer 11.
+ *
+ * https://github.com/sindresorhus/hex-rgb
+ */
 
-const nonHexChars = new RegExp(`[^#${hexChars}]`, 'gi');
-const validHexSize = new RegExp(`^${match3or4Hex}$|^${match6or8Hex}$`, 'i');
+const hexChars = 'a-f\\d'
+const match3or4Hex = `#?[${hexChars}]{3}[${hexChars}]?`
+const match6or8Hex = `#?[${hexChars}]{6}([${hexChars}]{2})?`
 
-function hexRgb(hex, options = {}) {
-	if (typeof hex !== 'string' || nonHexChars.test(hex) || !validHexSize.test(hex)) {
-		throw new TypeError('Expected a valid hex string');
-	}
+const nonHexChars = new RegExp(`[^#${hexChars}]`, 'gi')
+const validHexSize = new RegExp(`^${match3or4Hex}$|^${match6or8Hex}$`, 'i')
 
-	hex = hex.replace(/^#/, '');
-	let alpha = 255;
+function hexRgb(hexStr, options = {}) {
+  let hex = hexStr
+  if (typeof hex !== 'string' || nonHexChars.test(hex) || !validHexSize.test(hex)) {
+    throw new TypeError('Expected a valid hex string')
+  }
 
-	if (hex.length === 8) {
-		alpha = parseInt(hex.slice(6, 8), 16) / 255;
-		hex = hex.substring(0, 6);
-	}
+  hex = hex.replace(/^#/, '')
+  let alpha = 255
 
-	if (hex.length === 4) {
-		alpha = parseInt(hex.slice(3, 4).repeat(2), 16) / 255;
-		hex = hex.substring(0, 3);
-	}
+  if (hex.length === 8) {
+    alpha = parseInt(hex.slice(6, 8), 16) / 255
+    hex = hex.substring(0, 6)
+  }
 
-	if (hex.length === 3) {
-		hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
-	}
+  if (hex.length === 4) {
+    alpha = parseInt(hex.slice(3, 4).repeat(2), 16) / 255
+    hex = hex.substring(0, 3)
+  }
 
-	const num = parseInt(hex, 16);
-	const red = num >> 16;
-	const green = (num >> 8) & 255;
-	const blue = num & 255;
+  if (hex.length === 3) {
+    hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2]
+  }
 
-	return options.format === 'array' ?
-		[red, green, blue, alpha] :
-		{red, green, blue, alpha};
-};
+  const num = parseInt(hex, 16)
+  const red = num >> 16
+  const green = (num >> 8) & 255
+  const blue = num & 255
+
+  return options.format === 'array' ?
+    [red, green, blue, alpha] :
+    {red, green, blue, alpha}
+}
 
 export default function hexToRgba(hex, alpha = 1) {
   const rgb = hexRgb(hex, { format: 'array' })
