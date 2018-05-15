b9e364c6ba7c669854ccce8cffb2929354f6a0ec
Marco Segreto <msegreto@ideo.com>
2018-05-15 16:01:02 -0700

    remove some stuff from local storage mock

--- __mocks__/mobx-localstorage.js
+++ __mocks__/mobx-localstorage.js
@@ -1,14 +1,4 @@
 
 const localStorage = jest.genMockFromModule('mobx-localstorage')
-const store = {}
-
-localStorage.__setValue = (key, val) => {
-  store[key] = val
-}
-localStorage.__clear = () => {
-  store = {}
-}
-
-localStorage.__store = () => store
 
 export default localStorage
