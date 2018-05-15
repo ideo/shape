989a411b563d05e5506310c07bae73afcae68799
Marco Segreto <msegreto@ideo.com>
2018-05-14 18:39:15 -0700

    Spelling edit

--- __tests__/ui/activity_log/ActivityLogBox.unit.test.js
+++ __tests__/ui/activity_log/ActivityLogBox.unit.test.js
@@ -24,7 +24,7 @@ describe('ActivityLogBox', () => {
     })
   })
 
-  describe('componentDiMount()', () => {
+  describe('componentDidMount()', () => {
     it('should set the position based with defaults if not set', () => {
       expect(component.position.x).toEqual(0)
       expect(component.position.y).toEqual(83)
