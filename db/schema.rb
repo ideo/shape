0483a0fb69e658987ef58d0bf82a18fb63abc5f6
Dave Kaplan <kaptron@users.noreply.github.com>
2018-05-18 16:08:47 -0700

    Fix item caching (#151)
    * Working on fixing item caching
    
    * Sets up Item cached_attributes to work the same as Collection
    
    * Remove copied test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180426163602) do
+ActiveRecord::Schema.define(version: 20180518214410) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,6 +92,7 @@ ActiveRecord::Schema.define(version: 20180426163602) do
     t.string "url"
     t.jsonb "text_data"
     t.string "thumbnail_url"
+    t.jsonb "cached_attributes"
     t.index ["cloned_from_id"], name: "index_items_on_cloned_from_id"
   end
 
