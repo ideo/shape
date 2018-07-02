e1adb03e1f5e598b121ff398faefad14fde4f6ff
kaptron <dkaplan@ideo.com>
2018-07-02 14:12:06 -0700

    make eslint happier

--- app/javascript/ui/grid/blankContentTool/GridCardBlank.js
+++ app/javascript/ui/grid/blankContentTool/GridCardBlank.js
@@ -342,8 +342,11 @@ class GridCardBlank extends React.Component {
         <BctDropzone droppingFile={this.state.droppingFile} id="dropzone">
           {!this.state.loading && !this.state.droppingFile &&
             <div className="text">
-              <img src={bctIcons} alt="Vimeo, picture, youtube icons"
-                style={{ width: '80px' }} />
+              <img
+                src={bctIcons}
+                alt="dropzone icons"
+                style={{ width: '80px' }}
+              />
               <div className="top">Drag &amp; Drop</div>
               <div className="or">or</div>
               <div className="bottom">Browse</div>
