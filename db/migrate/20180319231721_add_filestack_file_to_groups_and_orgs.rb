214c6cfff1cd3ac89adf34d2d31a46fbc64658ad
kaptron <dkaplan@ideo.com>
2018-08-08 12:03:50 -0700

    Fixed old migration

--- db/migrate/20180319231721_add_filestack_file_to_groups_and_orgs.rb
+++ db/migrate/20180319231721_add_filestack_file_to_groups_and_orgs.rb
@@ -2,14 +2,14 @@ class AddFilestackFileToGroupsAndOrgs < ActiveRecord::Migration[5.1]
   def up
     remove_column :organizations, :pic_url_square
 
-    Group.add_filestack_file_column!
-    Organization.add_filestack_file_column!
+    add_column :groups, :filestack_file_id, :integer
+    add_column :organizations, :filestack_file_id, :integer
   end
 
   def down
     add_column :organizations, :pic_url_square, :string
 
-    Group.remove_filestack_file_column!
-    Organization.remove_filestack_file_column!
+    remove_column :groups, :filestack_file_id
+    remove_column :organizations, :filestack_file_id
   end
 end
