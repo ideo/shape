5e5490744d590a2b293136ec57fe458d50fe62cf
Marco Segreto <msegreto@ideo.com>
2018-08-09 15:51:15 -0700

    Move helper modal

---
+++ app/javascript/ui/users/MoveHelperModal.js
@@ -0,0 +1,117 @@
+import { observable, action, runInAction } from 'mobx'
+import { observer, PropTypes as MobxPropTypes } from 'mobx-react'
+import styled from 'styled-components'
+import Dialog from '@material-ui/core/Dialog'
+import DialogContent from '@material-ui/core/DialogContent'
+import Checkbox from '@material-ui/core/Checkbox'
+import FormControl from '@material-ui/core/FormControl'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+
+import {
+  FormButton,
+} from '~/ui/global/styled/forms'
+import v from '~/utils/variables'
+
+const SpecialDisplayHeading = styled.p`
+  text-align: center;
+  margin: 0;
+  margin-bottom: 30px;
+  line-height: 1.625rem;
+  font-family: ${v.fonts.sans};
+  font-size: 1.25rem;
+  font-weight: ${v.weights.book};
+  color: ${v.colors.blackLava};
+`
+
+const StyledDialog = styled(Dialog)`
+  .modal__paper {
+    text-align: center;
+    padding: 20px;
+    padding-top: 35px;
+    max-width: 445px;
+    min-width: 540px;
+    width: 100%;
+  }
+`
+
+@observer
+class MoveHelperModal extends React.Component {
+  @observable dontShowChecked = false
+  @observable isLoading = false
+  @observable submitted = false
+
+  @action handleDontShowCheck = (event) => {
+    this.dontShowChecked = event.target.checked
+  }
+
+  @action handleSubmit = (e) => {
+    e.preventDefault()
+    const { currentUser } = this.props
+    this.submitted = true
+    if (this.dontShowChecked) {
+      this.isLoading = true
+      currentUser.API_hideMoveHelper()
+        .finally(() => {
+          runInAction(() => { this.isLoading = false })
+        })
+    }
+  }
+
+  render() {
+    return (
+      <StyledDialog
+        classes={{ paper: 'modal__paper' }}
+        open={!this.submitted}
+        BackdropProps={{
+          invisible: true
+        }}
+      >
+        <DialogContent>
+          <form onSubmit={this.handleSubmit}>
+            <img
+              src="https://s3-us-west-2.amazonaws.com/assets.shape.space/move_helper_diagram.png"
+              alt="Diagram showing moving items between multiple collections"
+              style={{ width: '410px', marginBottom: '40px' }}
+            />
+            <SpecialDisplayHeading>
+              Did you know when moving, duplicating, or linking items, {' '}
+              you can navigate to another collection to place the items there?
+            </SpecialDisplayHeading>
+            <FormControl
+              component="fieldset"
+              required
+            >
+              <FormControlLabel
+                classes={{ label: 'form-control' }}
+                control={
+                  <Checkbox
+                    classes={{
+                      checked: 'checkbox--color',
+                    }}
+                    checked={this.dontShowChecked}
+                    onChange={this.handleDontShowCheck}
+                    value="yes"
+                  />
+                }
+                label="Thanks, please don't show me this message again."
+              />
+              <div style={{ height: '54px' }} />
+            </FormControl>
+
+            <div className="button--center">
+              <FormButton disabled={this.isLoading}>
+                Close
+              </FormButton>
+            </div>
+          </form>
+        </DialogContent>
+      </StyledDialog>
+    )
+  }
+}
+
+MoveHelperModal.propTypes = {
+  currentUser: MobxPropTypes.objectOrObservableObject.isRequired,
+}
+
+export default MoveHelperModal
