5e289ed2fb62a45fdd80d0b74ab94a7271e41f9e
re5et <re5etsmyth@gmail.com>
2018-08-27 13:38:32 -0700

    Update to webpack 4

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,16 +1,31 @@
-const { environment } = require('@rails/webpacker')
+'use strict'
+
+const {environment} = require('@rails/webpacker')
 const path = require('path')
-const { castArray, flow, mapValues } = require('lodash/fp')
-const webpack = require('webpack')
+const {castArray, identity, flow, mapValues} = require('lodash/fp')
+
+const DEV = process.env.RAILS_ENV === 'development'
+const root = p => path.resolve(__dirname, '..', '..', p)
 
 const prepend = item => array => [item, ...array]
 
-const prependBabelPolyfill = mapValues(
-  flow(castArray, prepend(path.resolve(__dirname, 'babelPolyfill.js')))
-)
+const addReactHotLoader = env => {
+  env.config.set('devtool', 'cheap-module-source-map')
+  return env
+}
 
-const addBabelPolyfill = env => {
-  env.entry.app = prependBabelPolyfill(env.entry).app
+const addBundleAnalyzerPlugin = env => {
+  const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
+    .BundleAnalyzerPlugin
+
+  env.plugins.insert(
+    'BundleAnalyze',
+    new BundleAnalyzerPlugin({
+      openAnalyzer: true,
+      analyzerMode: 'static',
+    })
+  )
+
   return env
 }
 
@@ -39,17 +54,52 @@ const addReactSVGLoader = env => {
   return env
 }
 
-const provideReact = env => {
-  env.plugins.set('Provide', new webpack.ProvidePlugin({
-    React: 'react'
-  }))
+const prependBabelPolyfill = mapValues(
+  flow(castArray, prepend(path.resolve(__dirname, 'babelPolyfill.js')))
+)
+
+const addBabelPolyfill = env => {
+  const entry = env.config.get('entry')
+  env.config.set('entry', prependBabelPolyfill(entry))
+  return env
+}
+
+const addTypescriptLoader = env => {
+  env.loaders.append('typescript', {
+    test: /\.(ts|tsx)?(\.erb)?$/,
+    use: [
+      {
+        loader: 'babel-loader',
+        options: {cacheDirectory: true},
+      },
+      {
+        loader: 'ts-loader',
+        options: {
+          configFile: root('tsconfig.json'),
+          experimentalWatchApi: true,
+          transpileOnly: true,
+        },
+      },
+    ],
+    exclude: /node_modules/,
+  })
+
+  return env
+}
+
+const addIdeoSSOExternal = env => {
+  env.config.set('externals.ideo-sso', 'IdeoSSO')
+
   return env
 }
 
 const updateEnvironment = flow(
-  addBabelPolyfill,
+  DEV ? addReactHotLoader : identity,
   addReactSVGLoader,
-  provideReact,
+  addBabelPolyfill,
+  addTypescriptLoader,
+  addIdeoSSOExternal,
+  process.env.ANALYZE ? addBundleAnalyzerPlugin : identity
 )
 
 module.exports = updateEnvironment(environment)
