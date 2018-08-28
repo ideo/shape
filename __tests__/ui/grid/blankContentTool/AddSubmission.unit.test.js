925c9cf224bf10ebed429e6c2332701818d7d01e
Marco Segreto <msegreto@ideo.com>
2018-08-28 12:03:22 -0700

    Add tests for add submission

---
+++ __tests__/ui/grid/blankContentTool/AddSubmission.unit.test.js
@@ -0,0 +1,57 @@
+import AddSubmission from '~/ui/grid/blankContentTool/AddSubmission'
+import Collection from '~/stores/jsonApi/Collection'
+import fakeUiStore from '#/mocks/fakeUiStore'
+import {
+  fakeCollection,
+  fakeCollectionCard
+} from '#/mocks/data'
+
+jest.mock('../../../../app/javascript/stores/index')
+jest.mock('../../../../app/javascript/stores/jsonApi/Collection')
+
+let props, wrapper, component
+const fakeEv = { preventDefault: jest.fn() }
+
+describe('GridCardBlank', () => {
+  beforeEach(() => {
+    fakeUiStore.viewingCollection = fakeCollection
+    fakeUiStore.viewingCollection.submissionTypeName = 'text'
+    props = {
+      uiStore: fakeUiStore,
+      parent_id: fakeCollectionCard.parent_id,
+      submissionSettings: {
+        type: 'text',
+        template: fakeCollection,
+      }
+    }
+    wrapper = shallow(
+      <AddSubmission.wrappedComponent {...props} />
+    )
+    component = wrapper.instance()
+  })
+
+  describe('render()', () => {
+    it('should say the submission type name', () => {
+      console.log(wrapper.debug())
+      expect(
+        wrapper.find('StyledBlankCreationTool h3').text()
+      ).toEqual('Add a new text')
+    })
+
+    it('should render a submission button', () => {
+      expect(wrapper.find('SubmissionButton').exists()).toBe(true)
+    })
+  })
+
+  describe('handleSubmission()', () => {
+    beforeEach(() => {
+      wrapper.find('SubmissionButton').simulate('click', fakeEv)
+    })
+
+    it('should create submission on the collection record', () => {
+      expect(Collection.createSubmission).toHaveBeenCalled()
+      expect(Collection.createSubmission).toHaveBeenCalledWith(
+        props.parent_id, props.submissionSettings)
+    })
+  })
+})
