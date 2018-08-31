d46c404fd9a9df8bfc1a90e6ee651ee44949ad37
re5et <re5etsmyth@gmail.com>
2018-08-31 15:11:28 -0700

    Replace model#assignRef with model#addReference in Notification

--- app/javascript/stores/jsonApi/Activity.js
+++ app/javascript/stores/jsonApi/Activity.js
@@ -1,6 +1,19 @@
+import { ReferenceType } from 'datx'
+import { action } from 'mobx'
 import BaseRecord from './BaseRecord'
+import Collection from '~/stores/jsonApi/Collection'
+import Item from '~/stores/jsonApi/Item'
 
 class Activity extends BaseRecord {
+
+  @action setTarget(value) {
+    const model = value.internalType === 'collections' ? Collection : Item
+    this.addReference('target', value, {
+      type: ReferenceType.TO_ONE,
+      model,
+    })
+  }
+
   static trackActivity(action, target) {
     const activity = new Activity({
       action: action,
