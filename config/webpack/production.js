9d6717da6a3bda392bcd49b26f929205e4ca4f19
kaptron <dkaplan@ideo.com>
2018-09-10 10:01:52 -0700

    IE11 uglify fix added in webpacker4, this line was breaking heroku push

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,6 +1,3 @@
 const environment = require('./environment')
 
-// https://github.com/rails/webpacker/issues/1235#issuecomment-362417050
-environment.plugins.get('UglifyJs').options.uglifyOptions.ecma = 5
-
 module.exports = environment.toWebpackConfig()
