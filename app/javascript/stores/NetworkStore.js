6ddd1e48b96a410e71fff6d48e28cc9fa0acb8b9
re5et <re5etsmyth@gmail.com>
2018-09-14 10:03:10 -0700

    Update loadOrganization / LoadPaymentMethods in NetworkStore

--- app/javascript/stores/NetworkStore.js
+++ app/javascript/stores/NetworkStore.js
@@ -1,5 +1,5 @@
 import { Collection } from 'datx'
-import { jsonapi } from 'datx-jsonapi'
+import { jsonapi, saveModel } from 'datx-jsonapi'
 import { first } from 'lodash'
 
 import * as networkModels from '~shared/api.network.v1'
@@ -7,19 +7,25 @@ import * as networkModels from '~shared/api.network.v1'
 class NetworkStore extends jsonapi(Collection) {
   static types = Object.keys(networkModels).filter(x => x.type)
 
-  loadOrganization(external_id) {
-    return this
-      .fetchAll('organizations', {
-        filter: {external_id}
-      }).then(response => first(response.data))
+  get organization() {
+    return first(this.findAll('organizations'))
   }
 
-  loadPaymentMethods(organization_id) {
-    return this
-      .fetchAll('payment_methods', {
-        sort: '-default,exp_year,exp_month',
-        filter: {organization_id},
-      }).then(response => response.data)
+  loadOrganization(external_id) {
+    return this.fetchAll('organizations', {
+      filter: {external_id}
+    })
+  }
+
+  loadPaymentMethods(organization_id, skipCache = false) {
+    return this.fetchAll('payment_methods', {
+      sort: '-default,exp_year,exp_month',
+      filter: {organization_id},
+    }, {
+      skipCache,
+    })
+  }
+
   createPaymentMethod(organization, token) {
     const {card} = token
     if (!card) {
