bb50f2f3089ee6960512ab7e4134e0437134e66b
re5et <re5etsmyth@gmail.com>
2018-09-14 10:02:41 -0700

    Add createPaymentMethod method to NetworkStore

--- app/javascript/stores/NetworkStore.js
+++ app/javascript/stores/NetworkStore.js
@@ -20,6 +20,29 @@ class NetworkStore extends jsonapi(Collection) {
         sort: '-default,exp_year,exp_month',
         filter: {organization_id},
       }).then(response => response.data)
+  createPaymentMethod(organization, token) {
+    const {card} = token
+    if (!card) {
+      throw new Error('Missing card')
+    }
+    if (!card.name) {
+      throw new Error('Missing card name')
+    }
+    if (!card.address_zip) {
+      throw new Error('Missing card address zip')
+    }
+
+    const paymentMethod = new networkModels.CreatePaymentMethod(
+      token.id,
+      card.name,
+      card.exp_month,
+      card.exp_year,
+      card.address_zip,
+      card.country,
+      false
+    )
+    paymentMethod.organization_id = organization.id
+    return saveModel(paymentMethod).then(() => this.add(paymentMethod))
   }
 }
 
