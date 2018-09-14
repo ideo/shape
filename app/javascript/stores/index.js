5006089d074a0e7a22f95b3bd704da708c0fb57b
re5et <re5etsmyth@gmail.com>
2018-09-14 10:00:59 -0700

    Update datx config to deal with different types of paths

--- app/javascript/stores/index.js
+++ app/javascript/stores/index.js
@@ -1,36 +1,48 @@
-import { config } from 'datx-jsonapi'
+import { config } from "datx-jsonapi"
 
-import ApiStore from './ApiStore'
-import NetworkStore from './NetworkStore'
-import RoutingStore from './RoutingStore'
-import UiStore from './UiStore'
+import ApiStore from "./ApiStore"
+import NetworkStore from "./NetworkStore"
+import RoutingStore from "./RoutingStore"
+import UiStore from "./UiStore"
 
-config.baseUrl = '/api/v1/'
-// modify fetch to include 'same-origin' credentials
+import * as networkModels from "~shared/api.network.v1"
 
-import * as networkModels from '~shared/api.network.v1'
-
-const networkApiBaseUrl = "https://profile.ideo.com/api/v1"
 const networkApiTypes = Object.values(networkModels)
-      .filter(x => x.type)
-      .map(x => x.type)
+  .filter(x => x.type)
+  .map(x => x.type)
 
-const getRequestType = (url) => {
-  const match = /.*\/([^\?]*)/.exec(url)
-  return match[1]
+const getRequestType = url => {
+  const segments = url.split("/")
+  let typeSegment = segments[segments.length - 1]
+  // we want the last segment unless it is an id
+  if (typeSegment.match(/^\d+$/)) {
+    typeSegment = segments[segments.length - 2]
+  }
+  // works for foo?bar=baz or foo
+  return typeSegment.split("?")[0]
 }
 
-const isNetworkType = (type) => ~networkApiTypes.indexOf(type)
+const isNetworkType = type => ~networkApiTypes.indexOf(type)
+
+config.baseUrl = "/api/v1/"
 
 config.fetchReference = (url, opts) => {
   const type = getRequestType(url)
   if (isNetworkType(type)) {
-    url = `${networkApiBaseUrl}/${type}`
-    opts.credentials = 'include'
+    url = url.replace(config.baseUrl, `${window.CONFIG.networkApiBaseUrl}/`)
+    opts.credentials = "include"
   } else {
-    opts.credentials = 'same-origin'
+    opts.credentials = "same-origin"
   }
   return fetch(url, opts)
+}
+
+config.transformRequest = options => {
+  options.options = options.options || {}
+  options.options.headers = options.options.headers || {}
+  options.options.headers["content-type"] = "application/vnd.api+json"
+  options.options.headers["client-id"] = window.CONFIG.clientId
+  return options
 }
 
 export const apiStore = new ApiStore()
@@ -42,5 +54,5 @@ export default {
   apiStore,
   networkStore,
   routingStore,
-  uiStore,
+  uiStore
 }
