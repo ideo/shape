1370402617c05fbcbb8309993f5499fe2064b2f7
Marco Segreto <msegreto@ideo.com>
2018-09-17 14:36:43 -0700

    Modified count of things in test
    Because there's another question item on test collection creation
    now so incremented by 1.

--- spec/models/collection/test_collection_spec.rb
+++ spec/models/collection/test_collection_spec.rb
@@ -9,8 +9,8 @@ describe Collection::TestCollection, type: :model do
     let(:test_collection) { create(:test_collection) }
 
     it 'should create the default setup with its attached cards and items' do
-      expect(test_collection.collection_cards.count).to eq 3
-      expect(test_collection.items.count).to eq 3
+      expect(test_collection.collection_cards.count).to eq 4
+      expect(test_collection.items.count).to eq 4
     end
   end
 end
