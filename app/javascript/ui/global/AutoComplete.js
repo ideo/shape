ca6060b33c70b06453ffacc2b617f1d6e0ea76cf
re5et <re5etsmyth@gmail.com>
2018-09-18 16:10:52 -0700

    Get Autocomplete component functional with updated react-select

--- app/javascript/ui/global/AutoComplete.js
+++ app/javascript/ui/global/AutoComplete.js
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 import { withStyles } from '@material-ui/core/styles'
 import Input from '@material-ui/core/Input'
 import Chip from '@material-ui/core/Chip'
-import Select from 'react-select'
+import Creatable from 'react-select/lib/Creatable'
 
 import Option from '~/ui/global/AutocompleteOption'
 import SearchIcon from '~/ui/icons/SearchIcon'
@@ -22,43 +22,73 @@ const SearchIconContainer = styled.span`
   }
 `
 
-function renderArrow() {
-  return (
-    <SearchIconContainer>
-      <SearchIcon />
-    </SearchIconContainer>
-  )
+const arrowRenderer = () => (
+  <SearchIconContainer>
+    <SearchIcon />
+  </SearchIconContainer>
+)
+
+const valueComponent = classes => valueProps => {
+  const { value, children, onRemove } = valueProps
+
+  const onDelete = event => {
+    event.preventDefault()
+    event.stopPropagation()
+    onRemove(value)
+  }
+
+  if (onRemove) {
+    return (
+      <Chip
+        tabIndex={-1}
+        label={children}
+        className={classes.chip}
+        onDelete={onDelete}
+      />
+    )
+  }
+
+  return <div className="Select-value">{children}</div>
+}
+
+const selectStyles = {
+  control: base => ({
+    ...base,
+    width: '370px',
+    paddingLeft: '24px',
+    display: 'flex',
+    alignItems: 'center',
+    border: 0,
+    height: 'auto',
+    background: 'transparent',
+    '&:hover': {
+      boxShadow: 'none',
+    },
+  }),
+  noOptionsMessage: base => ({
+    ...base,
+    // padding: theme.spacing.unit * 2,
+  }),
+  input: base => ({
+    ...base,
+    display: 'inline-flex !important',
+    padding: 0,
+    height: 'auto',
+    width: '370px',
+  }),
 }
 
 function SelectWrapped(props) {
   const { classes, ...other } = props
   return (
-    <Select.Creatable
-      arrowRenderer={renderArrow}
-      optionComponent={Option}
-      noResultsText={'No results found'}
-      valueComponent={valueProps => {
-        const { value, children, onRemove } = valueProps
-
-        const onDelete = event => {
-          event.preventDefault()
-          event.stopPropagation()
-          onRemove(value)
-        }
-
-        if (onRemove) {
-          return (
-            <Chip
-              tabIndex={-1}
-              label={children}
-              className={classes.chip}
-              onDelete={onDelete}
-            />
-          )
-        }
-
-        return <div className="Select-value">{children}</div>
+    <Creatable
+      styles={selectStyles}
+      components={{
+        valueComponent: valueComponent(classes),
+        arrowRenderer,
+        optionComponent: Option,
       }}
+      noResultsText={'No results found'}
       {...other}
     />
   )
@@ -72,7 +102,7 @@ SelectWrapped.propTypes = {
   }).isRequired,
 }
 
-const ITEM_HEIGHT = 48
+// const ITEM_HEIGHT = 48
 
 const styles = theme => ({
   root: {
@@ -90,102 +120,90 @@ const styles = theme => ({
   // We are waiting on https://github.com/JedWatson/react-select/issues/1679
   // to provide a better implementation.
   // Also, we had to reset the default style injected by the library.
-  '@global': {
-    '.Select-control': {
-      width: '370px',
-      paddingLeft: '24px',
-      display: 'flex',
-      alignItems: 'center',
-      border: 0,
-      height: 'auto',
-      background: 'transparent',
-      '&:hover': {
-        boxShadow: 'none',
-      },
-    },
-    '.Select-multi-value-wrapper': {
-      flexGrow: 1,
-      display: 'flex',
-      flexWrap: 'wrap',
-    },
-    '.Select--multi .Select-input': {
-      width: '370px',
-      margin: 0,
-    },
-    '.Select-noresults': {
-      padding: theme.spacing.unit * 2,
-    },
-    '.Select-input': {
-      display: 'inline-flex !important',
-      padding: 0,
-      height: 'auto',
-      width: '370px',
-    },
-    '.Select-input input': {
-      background: 'transparent',
-      border: 0,
-      padding: 0,
-      cursor: 'default',
-      display: 'inline-block',
-      fontFamily: 'inherit',
-      fontSize: 'inherit',
-      margin: 0,
-      outline: 0,
-      width: '370px',
-    },
-    '.Select-placeholder, .Select--single .Select-value': {
-      position: 'absolute',
-      top: 0,
-      left: '24px',
-      right: 0,
-      bottom: 0,
-      display: 'flex',
-      alignItems: 'center',
-      padding: 0,
-    },
-    '.Select-placeholder': {
-      opacity: 0.42,
-      color: theme.palette.common.black,
-    },
-    '.Select-menu-outer': {
-      backgroundColor: 'white',
-      border: 'none',
-      boxShadow: 'none',
-      position: 'absolute',
-      left: 0,
-      top: `calc(100% + ${theme.spacing.unit}px)`,
-      width: '370px',
-      zIndex: 2,
-      maxHeight: ITEM_HEIGHT * 3.5,
-    },
-    '.Select.is-focused:not(.is-open) > .Select-control': {
-      boxShadow: 'none',
-    },
-    '.Select-menu': {
-      maxHeight: ITEM_HEIGHT * 3.5,
-      overflowY: 'auto',
-      width: '100%',
-    },
-    '.Select-menu div': {
-      boxSizing: 'content-box',
-    },
-    '.Select-arrow-zone, .Select-clear-zone': {
-      color: theme.palette.action.active,
-      cursor: 'pointer',
-      height: 21,
-      width: 21,
-      zIndex: 1,
-    },
-    // Only for screen readers. We can't use display none.
-    '.Select-aria-only': {
-      position: 'absolute',
-      overflow: 'hidden',
-      clip: 'rect(0 0 0 0)',
-      height: 1,
-      width: 1,
-      margin: -1,
-    },
-  },
+  // '@global': {
+  //   '.Select-multi-value-wrapper': {
+  //     flexGrow: 1,
+  //     display: 'flex',
+  //     flexWrap: 'wrap',
+  //   },
+  //   '.Select--multi .Select-input': {
+  //     width: '370px',
+  //     margin: 0,
+  //   },
+  //   '.Select-noresults': {
+  //     padding: theme.spacing.unit * 2,
+  //   },
+  //   '.Select-input': {
+  //     display: 'inline-flex !important',
+  //     padding: 0,
+  //     height: 'auto',
+  //     width: '370px',
+  //   },
+  //   '.Select-input input': {
+  //     background: 'transparent',
+  //     border: 0,
+  //     padding: 0,
+  //     cursor: 'default',
+  //     display: 'inline-block',
+  //     fontFamily: 'inherit',
+  //     fontSize: 'inherit',
+  //     margin: 0,
+  //     outline: 0,
+  //     width: '370px',
+  //   },
+  //   '.Select-placeholder, .Select--single .Select-value': {
+  //     position: 'absolute',
+  //     top: 0,
+  //     left: '24px',
+  //     right: 0,
+  //     bottom: 0,
+  //     display: 'flex',
+  //     alignItems: 'center',
+  //     padding: 0,
+  //   },
+  //   '.Select-placeholder': {
+  //     opacity: 0.42,
+  //     color: theme.palette.common.black,
+  //   },
+  //   '.Select-menu-outer': {
+  //     backgroundColor: 'white',
+  //     border: 'none',
+  //     boxShadow: 'none',
+  //     position: 'absolute',
+  //     left: 0,
+  //     top: `calc(100% + ${theme.spacing.unit}px)`,
+  //     width: '370px',
+  //     zIndex: 2,
+  //     maxHeight: ITEM_HEIGHT * 3.5,
+  //   },
+  //   '.Select.is-focused:not(.is-open) > .Select-control': {
+  //     boxShadow: 'none',
+  //   },
+  //   '.Select-menu': {
+  //     maxHeight: ITEM_HEIGHT * 3.5,
+  //     overflowY: 'auto',
+  //     width: '100%',
+  //   },
+  //   '.Select-menu div': {
+  //     boxSizing: 'content-box',
+  //   },
+  //   '.Select-arrow-zone, .Select-clear-zone': {
+  //     color: theme.palette.action.active,
+  //     cursor: 'pointer',
+  //     height: 21,
+  //     width: 21,
+  //     zIndex: 1,
+  //   },
+  //   // Only for screen readers. We can't use display none.
+  //   '.Select-aria-only': {
+  //     position: 'absolute',
+  //     overflow: 'hidden',
+  //     clip: 'rect(0 0 0 0)',
+  //     height: 1,
+  //     width: 1,
+  //     margin: -1,
+  //   },
+  // },
 })
 
 class AutoComplete extends React.Component {
@@ -197,11 +215,9 @@ class AutoComplete extends React.Component {
     this.setState({
       multi,
     })
-    let fullOption = this.props.options.find(
-      option => option.value === multi || option.label === multi
-    )
+    let fullOption = this.props.options.find(x => x === multi)
     if (!fullOption || !fullOption.data) {
-      fullOption = Object.assign({}, { data: { custom: fullOption.value } })
+      fullOption = Object.assign({}, { data: { custom: multi.value } })
     }
     this.props.onOptionSelect(fullOption.data)
   }
