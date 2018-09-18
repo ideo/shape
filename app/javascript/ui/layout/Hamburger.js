ab02dcf96b6601b58d9dc6526603bdb9bbebf097
kaptron <dkaplan@ideo.com>
2018-09-18 13:31:34 -0700

    More eslint fixes

--- app/javascript/ui/layout/Hamburger.js
+++ app/javascript/ui/layout/Hamburger.js
@@ -6,17 +6,16 @@ const StyledHamburger = styled.div`
   margin-bottom: 15px;
   width: ${props => props.width}px;
   height: ${props => props.height}px;
-  background-image: url('https://firebasestorage.googleapis.com/v0/b/shape-marketing.appspot.com/o/marketing%2Fcommon%2Fhamburger_menu.svg?alt=media&token=c0d38053-1cce-409f-978a-8ca80a381046')
-}
+  background-image: url('https://firebasestorage.googleapis.com/v0/b/shape-marketing.appspot.com/o/marketing%2Fcommon%2Fhamburger_menu.svg?alt=media&token=c0d38053-1cce-409f-978a-8ca80a381046');
   background-position: center;
   background-repeat: no-repeat;
   background-size: contain;
 `
 
-const Hamburger = (props) => {
-  const hamburgerProps = { ...props }
-  return <StyledHamburger {...hamburgerProps} title="Menu" />
-}
+// const Hamburger = props => {
+//   const hamburgerProps = { ...props }
+//   return <StyledHamburger {...hamburgerProps} title="Menu" />
+// }
 
 StyledHamburger.propTypes = {
   width: PropTypes.number,
