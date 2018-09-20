3d0de5cd30226d0e213204e53f3bbe077e062916
re5et <re5etsmyth@gmail.com>
2018-09-20 11:44:47 -0700

    Consolidate dev scripts

--- dev.sh
+++ dev.sh
@@ -7,11 +7,13 @@ $DIR/script/dev-setup
 
 if ! [ -x "$(command -v ttab)" ]; then
   echo 'Error: ttab is not installed. See: https://www.npmjs.com/package/ttab' >&2
-  exit 1
+  echo 'using `heroku local -f Procfile.development`'
+  heroku local -f Procfile.development
+else
+  # echo 'Running rails server...'
+  ttab bin/rails server
+  # echo 'Starting heroku local to run webpack-dev-server...'
+  ttab heroku local webpack,worker -f Procfile.development
+  # echo 'Opening atom...'
+  ttab 'atom . && open http://localhost:3000'
 fi
-# echo 'Running rails server...'
-ttab bin/rails server
-# echo 'Starting heroku local to run webpack-dev-server...'
-ttab heroku local webpack,worker -f Procfile.development
-# echo 'Opening atom...'
-ttab 'atom . && open http://localhost:3000'
