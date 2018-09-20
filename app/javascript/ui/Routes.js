3bc3139a90f4bec2b1bfb5c3e090255747c80173
re5et <re5etsmyth@gmail.com>
2018-09-20 11:14:36 -0700

    Override material-ui typography theme to be consistent with shared
    This should be audited carefully because these typography styles are
    used internally by material-ui components, which may already be in use.

--- app/javascript/ui/Routes.js
+++ app/javascript/ui/Routes.js
@@ -4,7 +4,6 @@ import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 import WindowSizeListener from 'react-window-size-listener'
 import styled from 'styled-components'
 import { pxToRem } from '~shared/styles/utils'
-
 import ActivityLogBox from '~/ui/activity_log/ActivityLogBox'
 import DialogWrapper from '~/ui/global/modals/DialogWrapper'
 import ErrorBoundary from '~/ui/global/ErrorBoundary'
@@ -55,11 +54,62 @@ class Routes extends React.Component {
     typography: {
       // Use the Shape font instead of the default Roboto font.
       fontFamily: v.fonts.sans,
+      body1: {
+        fontSize: '1rem',
+      },
+      // heading-1
+      headline: {
+        textTransform: 'uppercase',
+        fontWeight: 500,
+        fontSize: pxToRem(32),
+        letterSpacing: pxToRem(1.2),
+      },
+      // heading-2
+      subheading: {
+        textTransform: 'uppercase',
+        fontWeight: 500,
+        fontSize: pxToRem(20),
+        letterSpacing: pxToRem(0.5),
+      },
+      // heading-3
+      title: {
+        display: 'inline',
+        fontSize: pxToRem(14),
+        fontWeight: 500,
+        textTransform: 'uppercase',
+        fontStyle: 'normal',
+        fontStretch: 'normal',
+        lineHeight: 1.5,
+        letterSpacing: pxToRem(0.5),
+      },
+      // heading-4
+      display1: {
+        fontSize: pxToRem(16),
+        letterSpacing: pxToRem(0.6),
+        textTransform: 'none',
+      },
       // instructional
       display2: {
         fontSize: pxToRem(14),
         letterSpacing: 'normal',
         lineHeight: pxToRem(18),
+      },
+      // label
+      display3: {
+        fontSize: pxToRem(12),
+        fontWeight: 500,
+        textTransform: 'uppercase',
+        letterSpacing: pxToRem(0.5),
+      },
+      // emphasis
+      display4: {
+        fontSize: pxToRem(40),
+        fontWeight: 300,
+        fontStyle: 'italic',
+      },
+      // paragraph
+      body2: {
+        fontSize: pxToRem(16),
       },
     },
     palette: {
