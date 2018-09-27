3a30bf24675a7668515dbb7352c02647ad52ed7c
re5et <re5etsmyth@gmail.com>
2018-09-27 15:27:49 -0700

    Add NetworkOrganizationUserSyncWorker

---
+++ app/workers/network_organization_user_sync_worker.rb
@@ -0,0 +1,45 @@
+class NetworkOrganizationUserSyncWorker
+  include Sidekiq::Worker
+
+  def perform(user_uid, org_id, role_name, action)
+    start_worker_process do
+      forkperform(user_uid, org_id, role_name, action)
+    end
+  end
+
+  def forkperform(user_uid, org_id, role_name, action)
+    action = action.to_sym
+    if action == :add
+      add_role(user_uid, org_id, role_name)
+    elsif action == :remove
+      remove_role(user_uid, org_id, role_name)
+    else
+      raise "NetworkOrganizationUserSyncWorker unsupported action: #{action}"
+    end
+  end
+
+  private
+
+  def add_role(user_uid, org_id, role_name)
+    network_organization = NetworkApi::Organization.find_by_external_id(org_id)
+    network_role = NetworkApi::Role.find_or_create_by_organization(
+      network_organization.id, role_name
+    )
+    NetworkApi::UsersRole.create_by_uid(
+      user_uid: user_uid,
+      role_id: network_role.id
+    )
+  end
+
+  def remove_role(user_uid, org_id, role_name)
+    network_organization = NetworkApi::Organization.find_by_external_id(org_id)
+    network_role = NetworkApi::Role.find_by_organization(
+      network_organization.id, role_name
+    )
+    return true if network_role.blank?
+    NetworkApi::UsersRole.remove_by_uid(
+      user_uid: user_uid,
+      role_id: network_role.id
+    )
+  end
+end
