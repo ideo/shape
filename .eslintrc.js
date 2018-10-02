f62d125d61ec072d551875e353da212f6d133ff3
re5et <re5etsmyth@gmail.com>
2018-10-02 10:45:23 -0700

    Update network setup to use models with embedded baseUrl

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,85 +1,87 @@
 module.exports = {
-  plugins: [
-    "react",
-    "jest",
-    "cypress",
-    "prettier"
-  ],
+  plugins: ['react', 'jest', 'cypress', 'prettier'],
   env: {
-    "jest/globals": true,
-    "cypress/globals": true,
+    'jest/globals': true,
+    'cypress/globals': true,
   },
   extends: [
-    "airbnb",
-    "standard",
-    "standard-react",
-    "plugin:prettier/recommended",
-    "prettier/react",
-    "prettier/standard"
+    'airbnb',
+    'standard',
+    'standard-react',
+    'plugin:prettier/recommended',
+    'prettier/react',
+    'prettier/standard',
   ],
-  parser: "babel-eslint",
+  parser: 'babel-eslint',
   parserOptions: {
-    "ecmaVersion": 7,
-    "sourceType": "module",
-    "ecmaFeatures": {
-        "jsx": true
-    }
+    ecmaVersion: 7,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
   globals: {
-    "__DEV__"      : false,
-    "__PROD__"     : false,
-    "__DEBUG__"    : false,
-    "fetch":   true,
-    "webpack": true,
-    "React":   true,
+    __DEV__: false,
+    __PROD__: false,
+    __DEBUG__: false,
+    fetch: true,
+    webpack: true,
+    React: true,
     // for enzyme tests
-    "shallow": true,
-    "render": true,
-    "mount": true,
-    "relPath": true,
-    "localStorage": true,
-    "FileReader": true,
-    "DOMParser": true,
+    shallow: true,
+    render: true,
+    mount: true,
+    relPath: true,
+    localStorage: true,
+    FileReader: true,
+    DOMParser: true,
+    IdeoSSO: true,
   },
   settings: {
-    "import/resolver": {
-      "babel-module": {},
-      "node": {}
+    'import/resolver': {
+      'babel-module': {},
+      node: {},
     },
-    "module-resolver": {},
-    "import/core-modules": ["styled-jsx/css"]
+    'module-resolver': {},
+    'import/core-modules': ['styled-jsx/css'],
   },
   rules: {
-    "arrow-parens": [0, "as-needed"],
+    'arrow-parens': [0, 'as-needed'],
     // because of all of our rails API snake_case variables
-    "camelcase": 0,
-    "func-names": 0,
-    "import/no-absolute-path": 0,
-    "import/extensions": 0,
-    "no-param-reassign": ["error", { "props": false }],
-    "no-underscore-dangle": 0,
-    "prettier/prettier": "error",
-    "radix": ["error", "as-needed"],
-    "space-before-function-paren": 0,
-    "jsx-a11y/anchor-is-valid": [ "error", {
-      "components": [ "Link" ],
-      "specialLink": [ "to", "hrefLeft", "hrefRight" ],
-      "aspects": [ "noHref", "invalidHref", "preferButton" ]
-    }],
-    "react-in-jsx-scope": 0,
-    "react/jsx-filename-extension": 0,
-    "react/prefer-stateless-function": 0,
-    "react/jsx-curly-brace-presence": 0,
-    "react/jsx-closing-tag-location": 0,
-    "react/no-did-mount-set-state": 0,
-    "react/sort-comp": [1, {
-      order: [
-        'static-methods',
-        'instance-variables',
-        'lifecycle',
-        'everything-else',
-        'render',
-      ]
-    }],
-  }
+    camelcase: 0,
+    'func-names': 0,
+    'import/no-absolute-path': 0,
+    'import/extensions': 0,
+    'no-param-reassign': ['error', { props: false }],
+    'no-underscore-dangle': 0,
+    'prettier/prettier': 'error',
+    radix: ['error', 'as-needed'],
+    'space-before-function-paren': 0,
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['to', 'hrefLeft', 'hrefRight'],
+        aspects: ['noHref', 'invalidHref', 'preferButton'],
+      },
+    ],
+    'react-in-jsx-scope': 0,
+    'react/jsx-filename-extension': 0,
+    'react/prefer-stateless-function': 0,
+    'react/jsx-curly-brace-presence': 0,
+    'react/jsx-closing-tag-location': 0,
+    'react/no-did-mount-set-state': 0,
+    'react/sort-comp': [
+      1,
+      {
+        order: [
+          'static-methods',
+          'instance-variables',
+          'lifecycle',
+          'everything-else',
+          'render',
+        ],
+      },
+    ],
+  },
 }
--- app/javascript/stores/index.js
+++ app/javascript/stores/index.js
@@ -2,46 +2,22 @@
 // (see cypress/support/commands.js)
 import 'whatwg-fetch'
 import { config } from 'datx-jsonapi'
-
-import * as networkModels from '~shared/api.network.v1'
-
 import ApiStore from './ApiStore'
 import NetworkStore from './NetworkStore'
 import RoutingStore from './RoutingStore'
 import UiStore from './UiStore'
 
-const getRequestType = url => {
-  const segments = url.split('/')
-  let typeSegment = segments[segments.length - 1]
-  // we want the last segment unless it is an id
-  if (typeSegment.match(/^\d+$/)) {
-    typeSegment = segments[segments.length - 2]
-  }
-  // works for foo?bar=baz or foo
-  return typeSegment.split('?')[0]
-}
-
-const networkApiTypes = Object.values(networkModels)
-  .filter(x => x.type)
-  .map(x => x.type)
-
-const isNetworkType = type => networkApiTypes.indexOf(type) > -1
+const isNetworkUrl = url => url.indexOf(IdeoSSO.baseApiUrl) === 0
 
 config.baseUrl = '/api/v1/'
 
 config.fetchReference = (url, opts) => {
-  let requestUrl = url
-  const type = getRequestType(url)
-  if (isNetworkType(type)) {
-    requestUrl = url.replace(
-      config.baseUrl,
-      `${window.CONFIG.networkApiBaseUrl}/`
-    )
+  if (isNetworkUrl(url)) {
     opts.credentials = 'include'
   } else {
     opts.credentials = 'same-origin'
   }
-  return fetch(requestUrl, opts)
+  return fetch(url, opts)
 }
 
 config.transformRequest = options => {
