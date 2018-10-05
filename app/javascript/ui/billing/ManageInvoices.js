372b3b53d939fabc81b2a9b0f09d3badbdf7ac6a
re5et <re5etsmyth@gmail.com>
2018-10-05 13:43:33 -0700

    Use Loader for ManageInvoices

--- app/javascript/ui/billing/ManageInvoices.js
+++ app/javascript/ui/billing/ManageInvoices.js
@@ -1,11 +1,16 @@
+import { observable, runInAction } from 'mobx'
 import { inject, observer, PropTypes as MobxPropTypes } from 'mobx-react'
 import trackError from '~/utils/trackError'
 import Invoices from '~shared/components/compounds/Invoices'
+import Loader from '~/ui/layout/Loader'
 import EmptyList from '~/ui/billing/EmptyList'
 
 @inject('apiStore', 'networkStore')
 @observer
 class ManagePaymentMethods extends React.Component {
+  @observable
+  loaded = false
+
   componentWillMount() {
     this.loadInvoices()
   }
@@ -20,12 +25,16 @@ class ManagePaymentMethods extends React.Component {
         await networkStore.loadInvoices(networkStore.subscription)
         this.forceUpdate()
       }
+      runInAction(() => (this.loaded = true))
     } catch (e) {
       trackError(e)
     }
   }
 
   render() {
+    if (!this.loaded) {
+      return <Loader />
+    }
     const invoices = this.props.networkStore.findAll('invoices')
     return (
       <Invoices
