278e154e8f649be4b5299bae55ec11c834e455b6
Marco Segreto <msegreto@ideo.com>
2018-10-11 11:12:05 -0700

    Move test logic to factory
    To make more reusable

--- spec/models/item/chart_item_spec.rb
+++ spec/models/item/chart_item_spec.rb
@@ -23,17 +23,8 @@ RSpec.describe Item::ChartItem, type: :model do
     context 'when the data source is a question item with responses' do
       let(:organization) { create(:organization) }
       let(:test_collection) { create(:test_collection, num_cards: 1, organization: organization) }
-      let(:other_test_collection) { create(:test_collection, organization: organization) }
-      let!(:org_survey_responses) { create_list(:survey_response, 5, test_collection: other_test_collection) }
+      let(:other_test_collection) { create(:test_collection, :with_responses, organization: organization) }
       let!(:survey_response) { create(:survey_response, test_collection: test_collection) }
-      let!(:org_question_items) do
-        org_survey_responses.map do |response|
-          question = response.question_items.select(&:question_useful?).first
-          create(:question_answer,
-                 survey_response: response,
-                 question: question)
-        end
-      end
       let!(:question_item) { survey_response.question_items.select(&:question_useful?).first }
       let!(:question_answer) do
         create(:question_answer,
@@ -45,12 +36,23 @@ RSpec.describe Item::ChartItem, type: :model do
 
       before do
         survey_response.update_attribute(:status, :completed)
-        org_survey_responses.map { |r| r.update_attribute(:status, :completed) }
         chart_item.update(data_source: question_item)
+        other_test_collection.reload
         card.update(item: chart_item)
       end
 
       it 'should return an array of question scale to amount' do
+        expect(chart_item.org_data).to eq(
+          label: test_collection.organization.name,
+          type: 'org_wide',
+          total: 6,
+          data: [
+            { num_responses: 6, answer: 1 },
+            { num_responses: 0, answer: 2 },
+            { num_responses: 0, answer: 3 },
+            { num_responses: 0, answer: 4 },
+          ],
+        )
         expect(chart_item.chart_data).to eq(
           datasets: [
             {
