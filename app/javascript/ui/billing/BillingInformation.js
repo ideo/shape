029d5d945b59953b859cb001aaa826bfcd95d44b
re5et <re5etsmyth@gmail.com>
2018-10-15 15:58:15 -0700

    Add styling to BillingInformation

--- app/javascript/ui/billing/BillingInformation.js
+++ app/javascript/ui/billing/BillingInformation.js
@@ -5,23 +5,54 @@ import { inject, observer, PropTypes as MobxPropTypes } from 'mobx-react'
 import styled from 'styled-components'
 import Section from '~shared/components/molecules/Section'
 import Typography from '~shared/components/atoms/Typography'
+import FancyDollarAmount from '~shared/components/atoms/FancyDollarAmount'
 import Box from '~shared/components/atoms/Box'
 import HorizontalDivider from '~shared/components/atoms/HorizontalDivider'
 import Loader from '~/ui/layout/Loader'
 import trackError from '~/utils/trackError'
 import { formatAsDollarAmount, formatDate } from '~shared/utils/formatters'
 
+const Wrapper = styled.div`
+  font-family: 'Gotham';
+`
+
+const Instructional = styled.div`
+  font-size: 12px;
+  line-height: 16px;
+  color: #c6c1bf;
+  letter-spacing: 0;
+`
+
+const BlockHeader = styled.h3`
+  font-size: 16px;
+  letter-spacing: 0.5px;
+  font-weight: 500;
+`
+
 const Block = ({ title, children }) => (
-  <Grid item xs={4}>
-    <Typography variant="label">{title}</Typography>
+  <Grid item xs={3}>
+    <BlockHeader>{title}</BlockHeader>
     <Box mt={20}>{children}</Box>
   </Grid>
 )
 
-const Prominent = styled.div`
-  font-family: 'Gotham';
-  font-size: 48px;
-  line-height: 22px;
+const TrialHighlight = styled.div`
+  background-color: #c0dbde;
+`
+
+const FreeTrial = styled(TrialHighlight)`
+  font-size: 16px;
+  line-height: 11px;
+  letter-spacing: 0.5px;
+  padding: 8px;
+  margin: 15px 0;
+  font-weight: 500;
+  display: inline-block;
+`
+
+const Label = styled.span`
+  font-size: 16px;
+  color: #a89f9b;
 `
 
 Block.propTypes = {
@@ -82,70 +113,91 @@ class BillingInformation extends React.Component {
       billableUserCount > 0 ? billableUserCount * price_per_user : 0
 
     return (
-      <Section title="Billing Information">
-        <Grid container spacing={40}>
-          <Block title="Current Active People">
-            <Grid container>
-              <Grid item>
-                <Prominent>{active_users_count}</Prominent>
+      <Wrapper>
+        <Section title="Billing Information">
+          <Grid container justify="space-between">
+            <Block title="Current Active People">
+              <Grid container justify="space-between" alignItems="flex-end">
+                <Grid item>
+                  <Typography variant="emphasis">
+                    {active_users_count}
+                  </Typography>
+                </Grid>
+                <Grid item>{is_within_trial_period && 'Total People'}</Grid>
               </Grid>
-              <Grid item>{is_within_trial_period && 'Total People'}</Grid>
-            </Grid>
-            <Grid container>
-              <Grid item>{trailsUsedCount}</Grid>
-              <Grid item> on free trial</Grid>
-            </Grid>
-            {is_within_trial_period &&
-              billableUserCount > 0 && (
+              {is_within_trial_period && (
                 <div>
+                  <TrialHighlight>
+                    <Box my={20} p={10}>
+                      <Grid container justify="space-between">
+                        <Grid item>{trailsUsedCount}</Grid>
+                        <Grid item>People on free trial</Grid>
+                      </Grid>
+                    </Box>
+                  </TrialHighlight>
                   <HorizontalDivider />
-                  <Grid container>
-                    <Grid item>{billableUserCount}</Grid>
-                    <Grid item>billable people</Grid>
+                  <Box p={10}>
+                    <Grid container justify="space-between">
+                      <Grid item>
+                        {billableUserCount < 1 ? 0 : billableUserCount}
+                      </Grid>
+                      <Grid item>Billable people</Grid>
+                    </Grid>
+                  </Box>
+                </div>
+              )}
+            </Block>
+            <Block title="Current Monthly Rate">
+              <FancyDollarAmount>{currentMonthlyRate}</FancyDollarAmount>
+              <Box my={25}>
+                {formatAsDollarAmount(price_per_user)}
+                /person per month
+              </Box>
+              <Instructional>
+                Total monthly rate does not include people covered by free trial
+                conditions.
+              </Instructional>
+            </Block>
+            <Block title="Current Billing Period">
+              <Grid container justify="space-between" spacing={16}>
+                <Grid item>
+                  <Label>Start date:</Label>
+                </Grid>
+                <Grid item>
+                  {formatDate(current_billing_period_start, 'MM/DD/YYYY')}
+                </Grid>
+              </Grid>
+              <Grid container justify="space-between" spacing={16}>
+                <Grid item>
+                  <Label>End date:</Label>
+                </Grid>
+                <Grid item>
+                  {formatDate(current_billing_period_end, 'MM/DD/YYYY')}
+                </Grid>
+              </Grid>
+              {is_within_trial_period && (
+                <div>
+                  <FreeTrial>FREE TRIAL</FreeTrial>
+                  <Grid container justify="space-between" spacing={16}>
+                    <Grid item>
+                      <Label>Free trials used:</Label>
+                    </Grid>
+                    <Grid item>
+                      {trailsUsedCount} of {trial_users_count}
+                    </Grid>
+                  </Grid>
+                  <Grid container justify="space-between" spacing={16}>
+                    <Grid item>
+                      <Label>Trial expires:</Label>
+                    </Grid>
+                    <Grid item>{trial_ends_at}</Grid>
                   </Grid>
                 </div>
               )}
-          </Block>
-          <Block title="Current Monthly Rate">
-            <Prominent>{formatAsDollarAmount(currentMonthlyRate)}</Prominent>
-            {formatAsDollarAmount(price_per_user)}
-            /person per month
-            <div>
-              Total monthly rate does not include people covered by free trial
-              conditions.
-            </div>
-          </Block>
-          <Block title="Current Billing Period">
-            <Grid container>
-              <Grid item>Start date:</Grid>
-              <Grid item>
-                {formatDate(current_billing_period_start, 'MM/DD/YYYY')}
-              </Grid>
-            </Grid>
-            <Grid container>
-              <Grid item>End date:</Grid>
-              <Grid item>
-                {formatDate(current_billing_period_end, 'MM/DD/YYYY')}
-              </Grid>
-            </Grid>
-            {is_within_trial_period && (
-              <div>
-                <div>FREE TRIAL</div>
-                <Grid container>
-                  <Grid item>Free trials used:</Grid>
-                  <Grid item>
-                    {trailsUsedCount} of {trial_users_count}
-                  </Grid>
-                </Grid>
-                <Grid container>
-                  <Grid item>Trial expires:</Grid>
-                  <Grid item>{trial_ends_at}</Grid>
-                </Grid>
-              </div>
-            )}
-          </Block>
-        </Grid>
-      </Section>
+            </Block>
+          </Grid>
+        </Section>
+      </Wrapper>
     )
   }
 }
