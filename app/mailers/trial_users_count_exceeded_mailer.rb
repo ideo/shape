be9bd5af6b688438bdf208818535b2c4fa7ca785
re5et <re5etsmyth@gmail.com>
2018-10-22 17:09:05 -0700

    Update and test TrialUsersCountExceededMailer

--- app/mailers/trial_users_count_exceeded_mailer.rb
+++ app/mailers/trial_users_count_exceeded_mailer.rb
@@ -1,7 +1,7 @@
 class TrialUsersCountExceededMailer < ApplicationMailer
   def notify(organization)
     emails = organization.admins[:users].map(&:email)
-    subject = 'Free trial limit preached'
+    subject = 'Free trial limit reached'
     @organization_name = organization.name
     @total_users_count = organization.active_users_count
     @trial_users_count = organization.trial_users_count
@@ -9,7 +9,8 @@ class TrialUsersCountExceededMailer < ApplicationMailer
     @next_monthly_charge = Organization::PRICE_PER_USER * @prorated_users_count
     @expiration_date = organization.trial_ends_at.to_s(:mdy)
     @next_statement_date = Time.now.utc.end_of_month.to_s(:mdy)
-    @missing_payment_method = NetworkApi::PaymentMethod.find(organization_id: organization.network_organization.id).empty?
+    payment_methods = NetworkApi::PaymentMethod.find(organization_id: organization.network_organization.id)
+    @missing_payment_method = payment_methods.empty?
     @url = "#{root_url}/billing"
     mail to: emails, subject: subject
   end
