8dd008a5afef7018258467a6636f0f4ffdcd0721
re5et <re5etsmyth@gmail.com>
2018-10-25 15:16:40 -0700

    Update network create usage record worker to notify billing changes

--- app/workers/network_create_usage_record_worker.rb
+++ app/workers/network_create_usage_record_worker.rb
@@ -2,6 +2,17 @@ class NetworkCreateUsageRecordWorker
   include Sidekiq::Worker
 
   def perform
-    Organization.find_each(&:create_network_usage_record)
+    Organization.find_each do |organization|
+      active_users_count = organization.active_users_count
+
+      next unless organization.create_network_usage_record &&
+                  organization.in_app_billing &&
+                  !organization.within_trial_period?
+
+      new_active_users_count = organization.reload.active_users_count - active_users_count
+      next unless new_active_users_count.positive?
+
+      BillingChangesMailer.notify(organization, new_active_users_count)
+    end
   end
 end
