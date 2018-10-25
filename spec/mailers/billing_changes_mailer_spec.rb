f39593824ba6e7127e2ce92157dec664b096708e
re5et <re5etsmyth@gmail.com>
2018-10-25 16:57:27 -0700

    Use include instead of match for email string expectation
    match converts to pattern, and the "$" screws it up for our purposes here.

--- spec/mailers/billing_changes_mailer_spec.rb
+++ spec/mailers/billing_changes_mailer_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe BillingChangesMailer, type: :mailer do
   let!(:network_organization) { double('network_organization', id: 123) }
   let!(:user) { create(:user) }
   let!(:payment_method) { [double('payment_method', user: user)] }
-  let!(:new_active_users) { 3 }
+  let!(:new_active_users) { 2 }
 
   before do
     allow(organization).to receive(:network_organization).and_return(network_organization)
@@ -37,14 +37,14 @@ RSpec.describe BillingChangesMailer, type: :mailer do
     it 'renders details about billing changes' do
       mail = BillingChangesMailer.notify(organization, new_active_users)
       expect(mail.body.encoded).to match("#{new_active_users} new users joined Shape for your organization: #{organization.name}")
-      expect(mail.body.encoded).to match("New users: #{new_active_users}")
-      expect(mail.body.encoded).to match("Total users: #{organization.active_users_count}")
-      expect(mail.body.encoded).to match("Additional Monthly Charge: #{format_currency(Organization::PRICE_PER_USER * new_active_users)}")
-      expect(mail.body.encoded).to match("New total monthly charge: #{format_currency(Organization::PRICE_PER_USER * organization.active_users_count)}")
+      expect(mail.body.encoded).to include("New users: #{new_active_users}")
+      expect(mail.body.encoded).to include("Total users: #{organization.active_users_count}")
+      expect(mail.body.encoded).to include("Additional Monthly Charge: #{format_currency(Organization::PRICE_PER_USER * new_active_users)}")
+      expect(mail.body.encoded).to include("New total monthly charge: #{format_currency(Organization::PRICE_PER_USER * organization.active_users_count)}")
       ppu = Organization::PRICE_PER_USER / ((Time.current.end_of_month.day - Time.current.day) + 1)
-      expect(mail.body.encoded).to match("Proprated charge this month: #{format_currency(new_active_users * ppu)}")
-      expect(mail.body.encoded).to match("Next statement date: #{Time.now.utc.end_of_month.to_s(:mdy)}")
-      expect(mail.body.encoded).to match("Go To Shape: #{root_url}")
+      expect(mail.body.encoded).to include("Prorated charge this month: #{format_currency(new_active_users * ppu)}")
+      expect(mail.body.encoded).to include("Next statement date: #{Time.now.utc.end_of_month.to_s(:mdy)}")
+      expect(mail.body.encoded).to include("Go To Shape: #{root_url}")
     end
   end
 end
