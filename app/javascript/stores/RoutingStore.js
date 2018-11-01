b0eda950b5a7e2dadc255735a08abf0e6fef3e81
Marco Segreto <msegreto@ideo.com>
2018-11-01 15:29:19 -0700

    Close org confirm dialog on page change

--- app/javascript/stores/RoutingStore.js
+++ app/javascript/stores/RoutingStore.js
@@ -28,6 +28,7 @@ class RoutingStore extends RouterStore {
     // close the org/roles menus if either are open when we route to a new page
     uiStore.update('organizationMenuPage', null)
     uiStore.update('rolesMenuOpen', null)
+    uiStore.closeDialog()
     if (!id && type !== 'homepage') {
       // in this case, type is a path like '/' or '/terms'
       this.push(type)
