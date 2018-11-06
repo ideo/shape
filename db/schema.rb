75786b303d6586e023a5f68152d6bc59deac18d8
re5et <re5etsmyth@gmail.com>
2018-11-06 14:22:43 -0800

    Add deactivated flag to organization

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181105224054) do
+ActiveRecord::Schema.define(version: 20181106211944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -239,6 +239,7 @@ ActiveRecord::Schema.define(version: 20181105224054) do
     t.boolean "sent_high_charges_low_email", default: false, null: false
     t.boolean "sent_high_charges_middle_email", default: false, null: false
     t.boolean "sent_high_charges_high_email", default: false, null: false
+    t.boolean "deactivated", default: false, null: false
     t.index ["slug"], name: "index_organizations_on_slug", unique: true
   end
 
