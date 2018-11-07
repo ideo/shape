655152f3759ea3bcc54ba1833315ca47649399bd
re5et <re5etsmyth@gmail.com>
2018-11-07 14:48:55 -0800

    Add billing menu and deactivate menu item

--- app/javascript/ui/pages/BillingPage.js
+++ app/javascript/ui/pages/BillingPage.js
@@ -1,22 +1,29 @@
+import { Grid } from '@material-ui/core'
+import Box from '~shared/components/atoms/Box'
 import { Heading1 } from '~/ui/global/styled/typography'
 import v from '~/utils/variables'
-import Header from '~/ui/layout/Header'
 import PageContainer from '~/ui/layout/PageContainer'
+import BillingMenu from '~/ui/billing/BillingMenu'
 import BillingInformation from '~/ui/billing/BillingInformation'
 import SuperAdminBillingControls from '~/ui/billing/SuperAdminBillingControls'
 import ReactivateAccount from '~/ui/billing/ReactivateAccount'
 import ManagePaymentMethods from '~/ui/billing/ManagePaymentMethods'
 import ManageInvoices from '~/ui/billing/ManageInvoices'
-import Box from '~shared/components/atoms/Box'
 import OverdueBanner from '~/ui/layout/OverdueBanner'
 
 class BillingPage extends React.Component {
   render() {
     return (
       <Box mb={v.headerHeightCompact}>
-        <Header />
         <PageContainer marginTop={v.headerHeightCompact}>
-          <Heading1>Billing</Heading1>
+          <Heading1>
+            <Grid container justify="space-between">
+              <Grid item>Billing</Grid>
+              <Grid item>
+                <BillingMenu />
+              </Grid>
+            </Grid>
+          </Heading1>
           <OverdueBanner />
           <SuperAdminBillingControls />
           <ReactivateAccount />
