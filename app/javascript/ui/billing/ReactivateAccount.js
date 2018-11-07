bb8162d5ae4302cf76f7332b22fa9bf991e80a69
re5et <re5etsmyth@gmail.com>
2018-11-07 14:04:07 -0800

    Add styling to reactivate account component

--- app/javascript/ui/billing/ReactivateAccount.js
+++ app/javascript/ui/billing/ReactivateAccount.js
@@ -1,10 +1,31 @@
 import { inject, observer, PropTypes as MobxPropTypes } from 'mobx-react'
 import { runInAction, observable } from 'mobx'
 import { Grid } from '@material-ui/core'
+import styled from 'styled-components'
 import Section from '~/ui/shared/components/molecules/Section'
 import AlertDialog from '~/ui/global/modals/AlertDialog'
+import Avatar from '~/ui/global/Avatar'
 import apiSaveModel from '~/utils/apiSaveModel'
 import trackError from '~/utils/trackError'
+import v from '~/utils/variables'
+
+const StyledHeader = styled.div`
+  font-size: 24px;
+  font-family: ${v.fonts.sans};
+  text-transform: uppercase;
+  font-weight: 500;
+`
+
+const StyledButton = styled.button`
+  background: ${v.colors.black};
+  color: ${v.colors.white};
+  border-radius: 19.5px;
+  padding: 10px 30px;
+  font-family: ${v.fonts.sans};
+  font-size: 16px;
+  font-weight: 500;
+  text-transform: uppercase;
+`
 
 @inject('apiStore')
 @observer
@@ -32,13 +53,7 @@ class ReactivateAccount extends React.Component {
   }
 
   render() {
-    const {
-      apiStore: {
-        currentUser: {
-          current_organization: { deactivated },
-        },
-      },
-    } = this.props
+    const organization = this.props.apiStore.currentUserOrganization
     if (this.alert) {
       return (
         <AlertDialog
@@ -49,23 +64,23 @@ class ReactivateAccount extends React.Component {
         />
       )
     }
-    if (!deactivated) {
+    if (!organization.deactivated) {
       return null
     }
     return (
       <Section>
-        <Grid container justify="space-between">
+        <Grid container justify="space-between" alignItems="center">
           <Grid item xs={1}>
-            logo
+            <Avatar url={organization.primary_group.filestack_file_url} />
           </Grid>
           <Grid item xs={7}>
-            This account is closed.
+            <StyledHeader>This account is closed.</StyledHeader>
           </Grid>
-          <Grid item xs={3} container justify="flex-end">
+          <Grid item xs={4} container justify="flex-end">
             <Grid item>
-              <button disabled={this.working} onClick={this.reactivate}>
-                Reactivate Account?
-              </button>
+              <StyledButton disabled={this.working} onClick={this.reactivate}>
+                Reactivate Account
+              </StyledButton>
             </Grid>
           </Grid>
         </Grid>
