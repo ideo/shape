bee9b5639c08e5a4719f9b0c3f12a3901a6a08d1
kaptron <dkaplan@ideo.com>
2018-12-03 13:17:37 -0800

    Fix Jest tests for new image url functions

--- __tests__/ui/grid/covers/ImageItemCover.unit.test.js
+++ __tests__/ui/grid/covers/ImageItemCover.unit.test.js
@@ -18,7 +18,7 @@ describe('ImageItemCover', () => {
 
   it('passes the url to StyledImageCover', () => {
     expect(wrapper.find('StyledImageCover').props().url).toEqual(
-      fakeImageItem.filestack_file_url
+      fakeImageItem.imageUrl()
     )
   })
 
