c3e46b9c2648031cef98de3f56e1e3a030712a64
kaptron <dkaplan@ideo.com>
2018-12-07 18:17:39 -0800

    Adds validations to data_item measure and timeframe values

--- app/models/item/data_item.rb
+++ app/models/item/data_item.rb
@@ -5,12 +5,34 @@ class Item
                    :d_filters,
                    :d_timeframe
 
+    validate :data_settings_validations
+    VALID_MEASURES = %w[
+      participants
+      viewers
+      activity
+    ].freeze
+    VALID_TIMEFRAMES = %w[
+      ever
+      month
+      week
+    ].freeze
+
     def report
       DataReport.new(self)
     end
 
     def data
       report.call
+    end
+
+    private
+
+    def data_settings_validations
+      unless VALID_MEASURES.include?(d_measure.to_s)
+        errors.add(:data_settings, "measure must be one of #{VALID_MEASURES.join(', ')}")
+      end
+      return if VALID_TIMEFRAMES.include?(d_timeframe.to_s)
+      errors.add(:data_settings, "timeframe must be one of #{VALID_TIMEFRAMES.join(', ')}")
     end
   end
 end
