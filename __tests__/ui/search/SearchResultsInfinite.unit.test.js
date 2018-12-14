a419286a27dd65fc252ba4c0a24258ac40905b0f
kaptron <dkaplan@ideo.com>
2018-12-14 15:42:32 -0800

    Fix SearchResultsInfinite unit test

--- __tests__/ui/search/SearchResultsInfinite.unit.test.js
+++ __tests__/ui/search/SearchResultsInfinite.unit.test.js
@@ -12,9 +12,9 @@ beforeEach(() => {
   props = {
     searchResults: [
       fakeCollection,
-      fakeCollection,
       fakeTextItem,
       fakeVideoItem,
+      fakeCollection,
       fakeLinkItem,
     ],
     gridSettings: { cols: 4 },
@@ -30,33 +30,16 @@ beforeEach(() => {
 
 describe('SearchResultsInfinite', () => {
   it('displays the search results', () => {
-    // 5 because 2 collections and 3 items
     expect(wrapper.find('StyledSearchResult').length).toEqual(5)
-    expect(
-      wrapper
-        .find('CollectionCover')
-        .at(0)
-        .props().collection
-    ).toEqual(fakeCollection)
-    expect(
-      wrapper
-        .find('GridCard')
-        .at(0)
-        .props().record
-    ).toEqual(fakeTextItem)
-    expect(
-      wrapper
-        .find('GridCard')
-        .at(1)
-        .props().record
-    ).toEqual(fakeVideoItem)
-  })
-
-  it('routes to collection on click', () => {
-    wrapper
-      .find('CollectionCover')
-      .at(0)
-      .simulate('click')
-    expect(props.routeTo).toBeCalledWith('collections', fakeCollection.id)
+    expect(wrapper.find('GridCard').length).toEqual(5)
+    const gridCards = [
+      wrapper.find('GridCard').at(0),
+      wrapper.find('GridCard').at(1),
+      wrapper.find('GridCard').at(2),
+    ]
+    expect(gridCards[0].props().record).toEqual(fakeCollection)
+    expect(gridCards[0].props().searchResult).toEqual(true)
+    expect(gridCards[1].props().record).toEqual(fakeTextItem)
+    expect(gridCards[2].props().record).toEqual(fakeVideoItem)
   })
 })
