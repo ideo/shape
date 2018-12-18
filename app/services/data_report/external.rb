d4b86d999ae7fb2971a86f0994ad532a11d7c52f
Matthew Villwock <mtvillwock@gmail.com>
2018-12-18 10:17:16 -0800

    Merge TV Dashboard updates

---
+++ app/services/data_report/external.rb
@@ -0,0 +1,36 @@
+module DataReport
+  class External < Base
+    def call
+      calculate
+      @data
+    end
+
+    private
+
+    def base_query
+      NetworkApi::AppMetric
+        .where(json_api_query_params)
+        .order(measured_at: :asc)
+    end
+
+    def json_api_query_params
+      params = @filters&.first || {}
+      {
+        measure: @measure,
+        start_date: 3.months.ago,
+        # timeframe: @timeframe
+      }.merge(params)
+    end
+
+    def calculate
+      values = base_query.map do |metric|
+        {
+          date: Date.parse(metric['measured_at']).to_s,
+          amount: metric['number'],
+        }
+      end
+
+      @data[:values] = values
+    end
+  end
+end
