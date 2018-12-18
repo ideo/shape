2b736a30ba896111da5eed79daf8084a57a47e36
mtvillwock <mtvillwock@gmail.com>
2018-12-17 16:35:37 -0800

    Replace util with lodash methods re: PR feedback

--- app/javascript/utils/stringUtils.js
+++
@@ -1,9 +0,0 @@
-// https://paulund.co.uk/how-to-capitalize-the-first-letter-of-a-string-in-javascript
-export function capitalizeSnakecase(string) {
-  return string
-    .split('_')
-    .map(substring => substring.charAt(0).toUpperCase() + substring.slice(1))
-    .join(' ')
-}
-
-export default { capitalizeSnakecase }
