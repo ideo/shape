ba0ff5c9da4bcd7cf1b27e84930613fe1c6727ce
kaptron <dkaplan@ideo.com>
2018-12-19 23:42:36 -0800

    Updates to BCT handling for collaborative editing
    - Your current BCT stays open even when new cards are created by other 
    editors

--- app/javascript/stores/jsonApi/CollectionCard.js
+++ app/javascript/stores/jsonApi/CollectionCard.js
@@ -112,8 +112,9 @@ class CollectionCard extends BaseRecord {
       const res = await this.apiStore.request('collection_cards', 'POST', {
         data: this.toJsonApi(),
       })
+      // important to close BCT before adding the new card so that the grid reflows properly
+      uiStore.closeBlankContentTool({ force: true })
       this.parentCollection.addCard(res.data)
-      uiStore.closeBlankContentTool()
       uiStore.trackEvent('create', this.parentCollection)
       return res.data
     } catch (e) {
