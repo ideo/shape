a56f97986347b3ec3e34384467a59389a9b35621
Marco Segreto <msegreto@ideo.com>
2019-01-16 13:18:55 -0800

    Fixed test, removed debugging code

--- app/models/collection_card.rb
+++ app/models/collection_card.rb
@@ -214,7 +214,7 @@ class CollectionCard < ApplicationRecord
         parent.queue_update_template_instances
       end
     end
-    CollectionCardArchiveWorker.new.perform(
+    CollectionCardArchiveWorker.perform_async(
       ids,
       # user_id is for the archive notification `actor`
       user_id,
