7a10f334f0ef6d8848d1d7fa25644b02bcc4938b
Marco Segreto <msegreto@ideo.com>
2019-01-24 13:21:36 -0800

    Remove unused array accessor

--- app/controllers/api/v1/comment_threads_controller.rb
+++ app/controllers/api/v1/comment_threads_controller.rb
@@ -95,7 +95,7 @@ class Api::V1::CommentThreadsController < Api::V1::BaseController
   end
 
   def toggle_subscribed(to_subscribe)
-    users_thread = @comment_thread.users_thread_for(current_user).first
+    users_thread = @comment_thread.users_thread_for(current_user)
     return false if users_thread.nil?
     users_thread.subscribed = to_subscribe
     users_thread.save
