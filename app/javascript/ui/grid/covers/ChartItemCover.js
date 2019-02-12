4af214ed03fb4ad8f8a0b0f2b4f6e261d3ff6dc0
Marco Segreto <msegreto@ideo.com>
2019-02-11 17:10:31 -0800

    Start on how chart item FE would be refactored

--- app/javascript/ui/grid/covers/ChartItemCover.js
+++ app/javascript/ui/grid/covers/ChartItemCover.js
@@ -13,11 +13,7 @@ import styled from 'styled-components'
 
 import { apiStore } from '~/stores'
 import { Heading1, Heading3 } from '~/ui/global/styled/typography'
-import {
-  questionInformation,
-  theme,
-  themeLabelStyles,
-} from '~/ui/test_collections/shared'
+import { theme, themeLabelStyles } from '~/ui/test_collections/shared'
 import Tooltip from '~/ui/global/Tooltip'
 import v from '~/utils/variables'
 
@@ -26,13 +22,13 @@ const CoverContainer = styled.div`
 `
 
 const Tick = props => {
-  const emoji = props.emojiScale[props.index]
-  if (!emoji) return <div />
-  const fontSize = parseInt((emoji.scale || 1) * 24)
+  const label = props.scale[props.index]
+  if (!label) return <div />
+  const fontSize = parseInt(1 * 24)
   return (
     <Tooltip
       classes={{ tooltip: 'Tooltip' }}
-      title={emoji.name}
+      title={label.title}
       placement="top"
       open={props.isHovered}
     >
@@ -44,11 +40,11 @@ const Tick = props => {
   )
 }
 Tick.propTypes = {
-  emojiScale: PropTypes.arrayOf(
+  scale: PropTypes.arrayOf(
     PropTypes.shape({
       name: PropTypes.string,
       symbol: PropTypes.string,
-      scale: PropTypes.number,
+      icon: PropTypes.string,
     })
   ).isRequired,
 }
@@ -56,63 +52,27 @@ Tick.propTypes = {
 @observer
 class ChartItemCover extends React.Component {
   @observable
-  question = null
+  data = null
 
   componentDidMount() {
-    this.fetchQuestionItem()
+    this.fetchData()
   }
 
-  async fetchQuestionItem() {
+  async fetchData() {
     const { item } = this.props
     const { data_source_id } = item
-    const questionResponse = await apiStore.fetch('items', data_source_id)
-    runInAction(() => (this.question = questionResponse.data))
-  }
-
-  get formattedData() {
-    const { item } = this.props
-    const raw = item.chart_data
-    const formatted = raw.datasets.map(set => {
-      const formattedPercentage = set.data.map(d => {
-        const percentage =
-          set.total > 0 ? parseInt((d.num_responses / set.total) * 100) : 0
-        return {
-          ...d,
-          percentage,
-          total: set.total,
-          type: set.type,
-        }
-      })
-      return { ...set, data: formattedPercentage }
-    })
-    return { datasets: formatted }
-  }
-
-  get mapQuestionType() {
-    const { questionTitle } = questionInformation(this.question)
-    let { questionText } = questionInformation(this.question)
-    if (this.question.question_type === 'question_category_satisfaction') {
-      questionText += ` ${this.question.content}`
-    }
-
-    return { questionTitle, questionText }
-  }
-
-  get emojiScale() {
-    if (!this.question) return []
-    const { emojiSeries } = questionInformation(this.question)
-    return emojiSeries
+    const data = await apiStore.fetch('items', data_source_id)
+    runInAction(() => (this.data = data.data))
   }
 
   render() {
-    const currentOrgName = apiStore.currentUserOrganization.name
-    const { testCollection } = this.props
+    const { item } = this.props
     return (
       <CoverContainer data-cy="ChartItemCover">
-        {this.question && (
+        {this.data && (
           <div>
-            <Heading1>{this.mapQuestionType.questionTitle}</Heading1>
-            <Heading3>{this.mapQuestionType.questionText}</Heading3>
+            <Heading1>{item.data_source.title}</Heading1>
+            <Heading3>{item.data_source.subtitle}</Heading3>
           </div>
         )}
         <VictoryChart
@@ -124,9 +84,9 @@ class ChartItemCover extends React.Component {
             style={{
               axis: { stroke: 'transparent' },
             }}
-            tickValues={[1, 2, 3, 4]}
-            tickFormat={this.emojiScale.map(e => e.symbol)}
-            tickLabelComponent={<Tick emojiScale={this.emojiScale} />}
+            tickValues={this.data.columns.map(col => col.title)}
+            tickFormat={this.data.columns.map(col => col.title)}
+            tickLabelComponent={<Tick scale={this.data.columns} />}
             events={[
               {
                 eventHandlers: {
@@ -149,17 +109,15 @@ class ChartItemCover extends React.Component {
             ]}
           />
           <VictoryGroup offset={30}>
-            {this.formattedData.datasets.map(d => (
+            {this.data.datasets.map(d => (
               <VictoryBar
                 key={d.type}
                 padding={10}
                 data={d.data}
                 barWidth={30}
-                x="answer"
-                y={datum => Math.max(datum.percentage, 0.5)}
-                labels={(datum, active) =>
-                  datum.type === 'question_items' ? `${datum.percentage}%` : ''
-                }
+                x="column"
+                y={datum => Math.max(datum.value, 0.5)}
+                labels={(datum, active) => datum.type === datum.value}
                 events={[
                   {
                     target: 'data',
@@ -170,14 +128,10 @@ class ChartItemCover extends React.Component {
                           mutation: props => {
                             const { datum } = props
                             return {
-                              text: `${datum.num_responses}/${
+                              text: `${datum.column_total}/${
                                 datum.total
                               } \ntotal`,
                               style: Object.assign({}, themeLabelStyles, {
-                                fill:
-                                  d.type === 'question_items'
-                                    ? v.colors.tertiaryMedium
-                                    : v.colors.primaryMediumDark,
                                 fontSize: 9,
                                 maxWidth: 20,
                               }),
@@ -206,10 +160,7 @@ class ChartItemCover extends React.Component {
             padding={0}
             rowGutter={0}
             style={{ fill: 'white' }}
-            data={[
-              { name: testCollection.name },
-              { name: `${currentOrgName} Organization` },
-            ]}
+            data={this.data.datasets.map(set => set.label)}
           />
         </VictoryChart>
       </CoverContainer>
@@ -219,7 +170,6 @@ class ChartItemCover extends React.Component {
 
 ChartItemCover.propTypes = {
   item: MobxPropTypes.objectOrObservableObject.isRequired,
-  testCollection: MobxPropTypes.objectOrObservableObject.isRequired,
 }
 
 export default ChartItemCover
