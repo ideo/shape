e7ab68f84dd3ce6c800fb51d6315e717179dc05a
Marco Segreto <msegreto@ideo.com>
2019-02-21 13:16:58 -0800

    Change data item test

--- cypress/support/step_definitions/then.js
+++ cypress/support/step_definitions/then.js
@@ -43,6 +43,11 @@ Then('I should see {int} for the single data value', num => {
   dataValue.should('contain', num)
 })
 
+Then('I should see the single data value', () => {
+  const dataValue = cy.get('[data-cy="DataReport-count"]')
+  dataValue.should('exist')
+})
+
 Then('I should see the {word} of {int} cards as {word}', (pos, count, size) => {
   // size e.g. "2x1" so we split on 'x'
   const sizes = size.split('x')
