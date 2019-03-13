849145fadb982d2e179edf7d86e47d368c70bc24
Marco Segreto <msegreto@ideo.com>
2019-03-13 15:49:19 -0700

    Hopefully complete heroku setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -116,7 +116,7 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
   #
-  config.action_cable.url = 'wss://ws.shape.space/cable'
+  config.action_cable.url = ENV['ACTION_CABLE_URL']
 
   if ENV['RAILS_LOG_TO_STDOUT'].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
