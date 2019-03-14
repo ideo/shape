9e40b1d013375fb84f2e44e511e3ac39b8103652
Marco Segreto <msegreto@ideo.com>
2019-03-14 09:55:16 -0700

    Fix mistake in code

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,4 @@
 
 Rails.application.config.session_store :cookie_store,
                                        key: '_any_cable_session',
-                                       domain: :any # or domain: '.example.com'
-
-# anywhere setting cookie
-cookies[:val] = { value: '1', domain: :any }
+                                       domain: '.shape.space' # or domain: '.example.com'
