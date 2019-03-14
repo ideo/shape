1b0dcabb587022d6fa7e19c0c93cee2960712035
Marco Segreto <msegreto@ideo.com>
2019-03-14 09:53:52 -0700

    Debugging

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 
 Rails.application.config.session_store :cookie_store,
                                        key: '_any_cable_session',
-                                       domain: '.shape.space' # or domain: '.example.com'
+                                       domain: :any # or domain: '.example.com'
 
 # anywhere setting cookie
-cookies[:val] = { value: '1', domain: '.shape.space' }
+cookies[:val] = { value: '1', domain: :any }
