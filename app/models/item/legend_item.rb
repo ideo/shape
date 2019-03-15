de1e83b6e514005409585bf7c92999f378609a7e
mtvillwock <mtvillwock@gmail.com>
2019-03-15 12:05:08 -0700

    Update legend item data set methods"

--- app/models/item/legend_item.rb
+++ app/models/item/legend_item.rb
@@ -2,14 +2,21 @@ class Item
   class LegendItem < Item
     has_many :data_items, class_name: 'Item::DataItem'
 
+    # TODO
+    # validate :link_item_to_legend?, if: :primary_measure_matches?
+
     def name
       "Compare To"
     end
 
     def primary_measure
-      # What about data_content.datasets.map { |data| data[:name] }
-      # How to reconcile datasets from three different chart types?
-      data_items.map(&:d_measure).compact.uniq
+      primary_datasets.first[:measure]
+    end
+
+    def primary_datasets
+      data_items
+        .map(&:datasets)
+        .select { |dataset| dataset[:primary] == true }
     end
 
     def active_comparisons
@@ -24,6 +31,17 @@ class Item
       # for each dataset
       # update their data_content/datasets
       # setting requested comparisons to be active
+    end
+
+    private
+
+    def link_to_legend(item)
+      item.update(legend_item_id: id)
+    end
+
+    def primary_measure_matches?(item)
+      dataset = item.datasets.find { |dataset| dataset[:primary] == true }
+      dataset[:measure] == primary_measure
     end
   end
 end
