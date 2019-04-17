7387bc7cbcdf1561de1cf3317565ec1092e2e44e
kaptron <dkaplan@ideo.com>
2019-04-16 23:03:16 -0700

    Simplified StyledImage centering

--- app/javascript/ui/items/ImageItem.js
+++ app/javascript/ui/items/ImageItem.js
@@ -5,15 +5,13 @@ import styled from 'styled-components'
 import { CloseButton } from '~/ui/global/styled/buttons'
 
 const StyledImage = styled.img`
-  /* Made it "responsive" and centered*/
   display: block;
+  /* basic way to make it "responsive" and centered */
   max-width: 100vw;
-  max-height: 42vw;
   margin-right: auto;
   margin-left: auto;
-  padding-top: 3%;
-
 `
+
 StyledImage.displayName = 'StyledImage'
 
 class ImageItem extends React.PureComponent {
