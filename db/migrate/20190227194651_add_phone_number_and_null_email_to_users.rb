459fb3a4186f7da2027214f338be050ab605ffa7
kaptron <dkaplan@ideo.com>
2019-05-03 10:10:38 -0700

    Fix irreversible migration 20190227194651

--- db/migrate/20190227194651_add_phone_number_and_null_email_to_users.rb
+++ db/migrate/20190227194651_add_phone_number_and_null_email_to_users.rb
@@ -1,6 +1,10 @@
 class AddPhoneNumberAndNullEmailToUsers < ActiveRecord::Migration[5.1]
-  def change
+  def up
     add_column :users, :phone, :string
     change_column :users, :email, :string, null: true
+  end
+  def down
+    remove_column :users, :phone
+    change_column :users, :email, :string, null: false
   end
 end
