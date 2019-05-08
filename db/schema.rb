2ddd75139a519bccd39a83034850b5d6a729b2af
kaptron <dkaplan@ideo.com>
2019-05-07 23:33:33 -0700

    Fix db/schema version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190503205644) do
+ActiveRecord::Schema.define(version: 20190426225125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
