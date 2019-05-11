a206970d70eb8e78b1348532aa5922249fc9c49b
Marco Segreto <msegreto@ideo.com>
2019-05-10 17:14:36 -0700

    Fix positioning of icons in comment box

--- app/javascript/ui/threads/CommentThreadHeader.js
+++ app/javascript/ui/threads/CommentThreadHeader.js
@@ -14,14 +14,14 @@ import TextIcon from '~/ui/icons/TextIcon'
 import Tooltip from '~/ui/global/Tooltip'
 import v, { ITEM_TYPES } from '~/utils/variables'
 
+const StyledLink = styled(Link)`
+  margin-right: 8px;
+`
+
 const StyledHeader = styled.div`
-  align-items: flex-start;
+  align-items: center;
   display: flex;
   height: ${props => (props.lines === 1 ? 50 : 70)}px;
-
-  *:first-child {
-    margin-right: 8px;
-  }
 
   .timestamp {
     margin-left: auto;
@@ -52,6 +52,10 @@ const StyledHeader = styled.div`
     &.show-unread .inner {
       opacity: 1;
     }
+  }
+
+  .icon {
+    margin-top: 12px;
   }
 `
 StyledHeader.displayName = 'StyledHeader'
@@ -155,22 +159,31 @@ class CommentThreadHeader extends React.Component {
   renderThumbnail() {
     const { record } = this
     let content
+    const iconPadding = this.titleLines === 1 ? 12 : 0
     if (record.internalType === 'items') {
       if (record.type === ITEM_TYPES.TEXT) {
-        content = <TextIcon viewBox="0 0 70 70" />
+        content = (
+          <div style={{ paddingTop: `${iconPadding}px` }}>
+            <TextIcon viewBox="0 0 70 70" />
+          </div>
+        )
       } else {
         content = <img src={record.filestack_file_url} alt="Text" />
       }
     } else {
-      content = <CollectionIcon viewBox="50 50 170 170" />
+      content = (
+        <div style={{ paddingTop: `${iconPadding}px` }}>
+          <CollectionIcon viewBox="50 50 170 170" />
+        </div>
+      )
       if (record.cover.image_url) {
         content = <img src={record.cover.image_url} alt={record.name} />
       }
     }
     return (
-      <Link to={this.objectLink()}>
+      <StyledLink to={this.objectLink()}>
         <ThumbnailHolder>{content}</ThumbnailHolder>
-      </Link>
+      </StyledLink>
     )
   }
 
