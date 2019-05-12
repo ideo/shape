8bead73968b5e229cb4439416bbd615fe19be42e
Cassie Koomjian <cassie@substantial.com>
2019-05-12 11:49:05 -0700

    Add admin pack, AdminRoutes & AdminPage

---
+++ app/javascript/ui/AdminRoutes.js
@@ -0,0 +1,82 @@
+import { inject, observer, PropTypes as MobxPropTypes } from 'mobx-react'
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
+import { MuiThemeProvider } from '@material-ui/core/styles'
+
+import AdminPage from '~/ui/pages/AdminPage'
+import DialogWrapper from '~/ui/global/modals/DialogWrapper'
+import ErrorBoundary from '~/ui/global/ErrorBoundary'
+import Header from '~/ui/layout/Header'
+import Loader from '~/ui/layout/Loader'
+import MuiTheme from '~/ui/theme'
+import SearchPage from '~/ui/pages/SearchPage'
+import SettingsPage from '~/ui/pages/SettingsPage'
+import UserSettings from '~/ui/users/UserSettings'
+import ZendeskWidget from '~/ui/global/ZendeskWidget'
+import firebaseClient from '~/vendor/firestore'
+import { CollectionApiWrapper } from '~/ui/pages/PageWithApiWrapper'
+
+// withRouter allows it to respond automatically to routing changes in props
+@withRouter
+@inject('apiStore')
+@observer
+class AdminRoutes extends React.Component {
+  componentDidMount() {
+    const { apiStore } = this.props
+    apiStore.loadCurrentUser({
+      onSuccess: currentUser => {
+        firebaseClient.authenticate(currentUser.google_auth_token)
+      },
+    })
+  }
+
+  render() {
+    const { apiStore } = this.props
+    const { sessionLoaded } = apiStore
+    if (!sessionLoaded) {
+      return <Loader />
+    }
+    return (
+      <ErrorBoundary>
+        <MuiThemeProvider theme={MuiTheme}>
+          <DialogWrapper />
+          <ZendeskWidget />
+
+          <Header />
+
+          {/* Switch will stop when it finds the first matching path */}
+          <Switch>
+            <Route path="/admin" component={AdminPage} />
+            <Route path="/search" component={SearchPage} />
+            <Route
+              path="/user_settings"
+              render={() => (
+                <SettingsPage>
+                  <UserSettings />
+                </SettingsPage>
+              )}
+            />
+            {/* catch routes that we don't understand */}
+            {/* TODO: refactor PageError to be a more standalone 404 page */}
+            <Route
+              exact
+              path="/:org/:not_found"
+              render={() => <Redirect to="/" />}
+            />
+            {/* have to put this last to catch all org slugs */}
+            <Route
+              exact
+              path="/:org"
+              render={props => <CollectionApiWrapper {...props} />}
+            />
+          </Switch>
+        </MuiThemeProvider>
+      </ErrorBoundary>
+    )
+  }
+}
+
+AdminRoutes.wrappedComponent.propTypes = {
+  apiStore: MobxPropTypes.objectOrObservableObject.isRequired,
+}
+
+export default AdminRoutes
