9b60f76350aa3ba8418b1cde25e91326c74d5b62
Cassie Koomjian <cassie@substantial.com>
2019-05-13 14:52:50 -0700

    Add search bar to AdminHeader

--- app/javascript/ui/admin/AdminHeader.js
+++ app/javascript/ui/admin/AdminHeader.js
@@ -1,3 +1,4 @@
+import Hidden from '@material-ui/core/Hidden'
 import styled from 'styled-components'
 import { inject, observer, PropTypes as MobxPropTypes } from 'mobx-react'
 import { Fragment } from 'react'
@@ -8,6 +9,7 @@ import {
   MaxWidthContainer,
   HeaderSpacer,
 } from '~/ui/global/styled/layout'
+import GlobalSearch from '~/ui/layout/GlobalSearch'
 import Logo from '~/ui/layout/Logo'
 import PlainLink from '~/ui/global/PlainLink'
 import v from '~/utils/variables'
@@ -46,6 +48,12 @@ class AdminHeader extends React.Component {
 
               <Box auto>
                 <StyledHeadingWrapper>Shape Admin</StyledHeadingWrapper>
+              </Box>
+
+              <Box flex align="center">
+                <Hidden smDown>
+                  <GlobalSearch className="search-bar" />
+                </Hidden>
               </Box>
             </Flex>
           </MaxWidthContainer>
