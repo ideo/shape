8360af36286348b077f4caafc43269f0de12f91e
Cassie Koomjian <cassie@substantial.com>
2019-05-15 09:17:06 -0700

    Allow super admin to access Shape admin

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,6 +2,6 @@ class Admin::DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    raise CanCan::AccessDenied unless current_user.has_role?(Role::SHAPE_ADMIN)
+    raise CanCan::AccessDenied unless current_user.has_cached_role?(Role::SHAPE_ADMIN) || current_user.has_cached_role?(Role::SUPER_ADMIN)
   end
 end
