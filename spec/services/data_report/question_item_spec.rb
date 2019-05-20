fa42a4063358777b7733699708ddf100981daa5c
Josh Schwartzman <josh@dreamseedo.org>
2019-05-20 13:47:28 -0700

    WIP: updating questoin item data report

--- spec/services/data_report/question_item_spec.rb
+++ spec/services/data_report/question_item_spec.rb
@@ -3,107 +3,95 @@ require 'rails_helper'
 RSpec.describe DataReport::QuestionItem, type: :service do
   let(:organization) { create(:organization) }
   let(:collection) { create(:collection, organization: organization) }
-  let(:question_item) { create(:question_item) }
-  let(:data_item) { create(:data_item, :report_type_question_item, data_source: question_item) }
-  let!(:parent_collection_card) do
-    create(:collection_card,
-           parent: collection,
-           item: data_item)
+
+  context 'no responses' do
+    let(:empty_dataset) do
+      [
+        { column: 1, value: 0, percentage: 0 },
+        { column: 2, value: 0, percentage: 0 },
+        { column: 3, value: 0, percentage: 0 },
+        { column: 4, value: 0, percentage: 0 },
+      ]
+    end
+    let(:question_item) { create(:question_item) }
+    let(:dataset) { create(:question_item_dataset, data_source: question_item) }
+
+    describe '#call' do
+      it 'returns datasets for question item' do
+        expect(DataReport::QuestionItem.call(dataset: dataset)).to match_array(empty_dataset)
+      end
+    end
   end
-  let(:empty_dataset) do
-    [
-      { column: 1, value: 0, percentage: 0 },
-      { column: 2, value: 0, percentage: 0 },
-      { column: 3, value: 0, percentage: 0 },
-      { column: 4, value: 0, percentage: 0 },
-    ]
-  end
 
-  subject {
-    DataReport::QuestionItem.call(data_item)
-  }
-
-  describe '#call' do
-    it 'returns datasets for question item' do
-      expect(subject[0].except(:data)).to eq(
-        order: 0,
-        chart_type: 'bar',
-        measure: collection.name,
-        question_type: question_item.question_type,
-        total: 0,
-        timeframe: 'month',
-        max_domain: 95,
-      )
-      expect(subject[0][:data]).to match_array(empty_dataset)
-
-      expect(subject[1].except(:data)).to eq(
-        order: 1,
-        chart_type: 'bar',
-        measure: organization.name",
-        question_type: question_item.question_type,
-        total: 0,
-        timeframe: 'month',
-        max_domain: 95,
-      )
-      expect(subject[1][:data]).to match_array(empty_dataset)
+  context 'with responses' do
+    let(:test_collection) { create(:test_collection, num_cards: 1, organization: organization) }
+    # Create another test collection so we have other responses in the org
+    let(:other_test_collection) { create(:test_collection, :with_responses, organization: organization) }
+    let!(:survey_response) { create(:survey_response, test_collection: test_collection) }
+    let!(:question_item) { survey_response.question_items.select(&:question_useful?).first }
+    let!(:question_answer) do
+      create(:question_answer,
+             survey_response: survey_response,
+             question: question_item)
     end
 
-    context 'with responses' do
-      let(:test_collection) { create(:test_collection, num_cards: 1, organization: organization) }
-      # Create another test collection so we have other responses in the org
-      let(:other_test_collection) { create(:test_collection, :with_responses, organization: organization) }
-      let!(:survey_response) { create(:survey_response, test_collection: test_collection) }
-      let!(:question_item) { survey_response.question_items.select(&:question_useful?).first }
-      let!(:question_answer) do
-        create(:question_answer,
-               survey_response: survey_response,
-               question: question_item)
-      end
-      let!(:data_item) { create(:data_item, :report_type_question_item, data_source: question_item) }
-      let(:card) { test_collection.collection_cards.first }
+    before do
+      survey_response.update_attribute(:status, :completed)
+      other_test_collection.reload
+    end
 
-      before do
-        survey_response.update_attribute(:status, :completed)
-        other_test_collection.reload
-        card.update(item: data_item)
+    context 'question dataset' do
+      let!(:dataset) do
+        create(:question_item_dataset, data_source: question_item)
       end
 
-      it 'should return an array of question scale to amount' do
-        expect(subject[0].except(:data)).to eq(
-          order: 0,
-          chart_type: 'bar',
-          measure: test_collection.name,
-          question_type: question_item.question_type,
-          total: 1,
-          timeframe: 'month',
-          max_domain: 95,
-        )
-        expect(subject[0][:data]).to match_array(
-          [
-            { column: 1, value: 1, percentage: 100 },
-            { column: 2, value: 0, percentage: 0 },
-            { column: 3, value: 0, percentage: 0 },
-            { column: 4, value: 0, percentage: 0 },
-          ]
-        )
+      describe '#call' do
+        it 'returns data for question' do
+          expect(DataReport::QuestionItem.call(dataset: dataset)).to match_array(
+            [
+              { column: 1, value: 1, percentage: 100 },
+              { column: 2, value: 0, percentage: 0 },
+              { column: 3, value: 0, percentage: 0 },
+              { column: 4, value: 0, percentage: 0 },
+            ]
+          )
+        end
+      end
 
-        expect(subject[1].except(:data)).to eq(
-          order: 1,
-          chart_type: 'bar',
-          measure: organization.name,
-          question_type: question_item.question_type,
-          total: 6,
-          timeframe: 'month',
-          max_domain: 95,
-        )
-        expect(subject[1][:data]).to match_array(
-          [
-            { column: 1, value: 6, percentage: 100 },
-            { column: 2, value: 0, percentage: 0 },
-            { column: 3, value: 0, percentage: 0 },
-            { column: 4, value: 0, percentage: 0 },
-          ]
-        )
+      describe '#total' do
+        it 'returns 1' do
+          expect(
+            DataReport::QuestionItem.new(dataset: dataset).total
+          ).to eq(1)
+        end
+      end
+    end
+
+    context 'org-wide dataset' do
+      let!(:dataset) do
+        create(:org_wide_question_dataset,
+               question_type: question_item.question_type)
+      end
+
+      describe '#call' do
+        it 'returns org-wide data' do
+          expect(DataReport::QuestionItem.call(dataset: dataset)).to match_array(
+            [
+              { column: 1, value: 6, percentage: 100 },
+              { column: 2, value: 0, percentage: 0 },
+              { column: 3, value: 0, percentage: 0 },
+              { column: 4, value: 0, percentage: 0 },
+            ]
+          )
+        end
+      end
+
+      describe '#total' do
+        it 'returns 6' do
+          expect(
+            DataReport::QuestionItem.new(dataset: dataset).total
+          ).to eq(6)
+        end
       end
     end
   end
