a2315f62a3e8ab010b3c653382707d4899abe8c9
Cassie Koomjian <cassie@substantial.com>
2019-05-23 17:44:22 -0700

    Add control for adding audience criteria

--- app/javascript/ui/test_collections/AudienceSettings/AddAudienceModal.js
+++ app/javascript/ui/test_collections/AudienceSettings/AddAudienceModal.js
@@ -1,16 +1,44 @@
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 import { inject, observer, PropTypes as MobxPropTypes } from 'mobx-react'
+import { Collapse } from '@material-ui/core'
 import { Grid } from '@material-ui/core'
 
+import Audience from '~/stores/jsonApi/Audience'
+import Button from '~shared/components/atoms/Button'
+import HorizontalDivider from '~shared/components/atoms/HorizontalDivider'
 import Modal from '~/ui/global/modals/Modal'
+import PlusIcon from '~shared/images/icon-plus.svg'
+import v from '~/utils/variables'
 import {
   FormButton,
   FieldContainer,
   Label,
+  SelectOption,
   TextButton,
   TextField,
 } from '~/ui/global/styled/forms'
-import Audience from '~/stores/jsonApi/Audience'
+
+// TODO Position menu on all viewport sizes
+const AddCriteriaMenu = styled.ul`
+  background: ${v.colors.white};
+  box-shadow: 0 0 8px 0 rgba(18,15,14,0.2);
+  left: calc(50% - 300px);
+  position: fixed;
+  top: calc(50% + 73px);
+  width: 250px;
+  z-index: 1500;gg
+`
+
+const CriteriaGroup = styled.li`
+  border-top: 1px solid ${v.colors.black};
+  font-size: 0.8125rem;
+  font-family: ${v.fonts.sans};
+  font-weight: ${v.weights.medium};
+  letter-spacing: 1px;
+  padding: 12px 10px 12px 20px;
+  text-transform: uppercase;
+`
 
 @inject('apiStore')
 @observer
@@ -18,6 +46,20 @@ class AddAudienceModal extends React.Component {
   state = {
     name: '',
     valid: false,
+    criteriaMenuOpen: false,
+  }
+
+  closeModal = () => {
+    this.closeCriteriaMenu()
+    this.props.close()
+  }
+
+  closeCriteriaMenu = () => {
+    this.setState({ criteriaMenuOpen: false })
+  }
+
+  toggleCriteriaMenu = () => {
+    this.setState({ criteriaMenuOpen: !this.state.criteriaMenuOpen })
   }
 
   handleNameChange = ev => {
@@ -45,42 +87,70 @@ class AddAudienceModal extends React.Component {
   }
 
   render() {
-    const { open, close } = this.props
-
     return (
-      <Modal title="Create New Audience" onClose={close} open={open} noScroll>
-        <FieldContainer>
-          <Label htmlFor="audienceName">Audience Name</Label>
-          <TextField
-            id="audienceName"
-            type="text"
-            value={this.state.name}
-            onChange={this.handleNameChange}
-            placeholder={'Enter Audience Name…'}
-          />
-        </FieldContainer>
-        <Grid container alignItems="center" style={{ paddingBottom: '32px' }}>
-          <Grid item xs={6}>
-            <Grid container justify="center">
-              <TextButton onClick={this.reset} width={190}>
-                Cancel
-              </TextButton>
+      <React.Fragment>
+        <Modal
+          title="Create New Audience"
+          onClose={this.closeModal}
+          open={this.props.open}
+          noScroll
+        >
+          <FieldContainer>
+            <Label htmlFor="audienceName">Audience Name</Label>
+            <TextField
+              id="audienceName"
+              type="text"
+              value={this.state.name}
+              onChange={this.handleNameChange}
+              placeholder={'Enter Audience Name…'}
+            />
+          </FieldContainer>
+          <FieldContainer>
+            <Label>Targeting Criteria</Label>
+            <Button href="#" onClick={this.toggleCriteriaMenu}>
+              <PlusIcon width={15} style={{ fill: v.colors.black }} />
+              Add Audience Criteria
+            </Button>
+            <HorizontalDivider
+              color={v.colors.commonMedium}
+              style={{ borderWidth: '0 0 1px 0' }}
+            />
+          </FieldContainer>
+          <Grid container alignItems="center" style={{ paddingBottom: '32px' }}>
+            <Grid item xs={6}>
+              <Grid container justify="center">
+                <TextButton onClick={this.reset} width={190}>
+                  Cancel
+                </TextButton>
+              </Grid>
+            </Grid>
+            <Grid item xs={6}>
+              <Grid container justify="center">
+                <FormButton
+                  onClick={this.handleSave}
+                  width={190}
+                  type="submit"
+                  disabled={!this.state.valid}
+                >
+                  Save
+                </FormButton>
+              </Grid>
             </Grid>
           </Grid>
-          <Grid item xs={6}>
-            <Grid container justify="center">
-              <FormButton
-                onClick={this.handleSave}
-                width={190}
-                type="submit"
-                disabled={!this.state.valid}
-              >
-                Save
-              </FormButton>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Modal>
+        </Modal>
+        <Collapse in={this.state.criteriaMenuOpen} timeout="auto" unmountOnExit>
+          <AddCriteriaMenu>
+            <CriteriaGroup>Demographics</CriteriaGroup>
+            <SelectOption classes={{ root: 'selectOption' }}>Age</SelectOption>
+            <SelectOption classes={{ root: 'selectOption' }}>
+              Children
+            </SelectOption>
+            <SelectOption classes={{ root: 'selectOption' }}>
+              Country
+            </SelectOption>
+          </AddCriteriaMenu>
+        </Collapse>
+      </React.Fragment>
     )
   }
 }
