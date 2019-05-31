30984d47e0c2c7fbbf908e71d7c92b5f0209e31a
kaptron <dkaplan@ideo.com>
2019-05-31 16:10:27 -0700

    Fix OrgTemplates error catch

--- app/services/organization_templates.rb
+++ app/services/organization_templates.rb
@@ -14,7 +14,7 @@ class OrganizationTemplates < SimpleService
     # NOTE: this also populates the first org admin's My Collection
     if @org.getting_started_collection.present? || getting_started_template.blank?
       # kind of an escape hatch in case your env does not have a valid getting_started_template
-      user_collection.update(awaiting_first_user_content: false)
+      @user&.current_user_collection&.update(awaiting_first_user_content: false)
       return
     end
 
