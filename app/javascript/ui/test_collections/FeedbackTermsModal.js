cf5ffe42d2c60bf3004a396ddd60e8dd15c45e96
mtvillwock <mtvillwock@gmail.com>
2019-06-04 11:22:25 -0700

    Make feedback modal styling better match Invision
    Make the modal look like this: https://ideo.invisionapp.com/d/main#/console/14424671/360618724/inspect
    
    Bug reported here: https://www.shape.space/ideo/collections/356300
    Also, the styling was kind of rushed when this was implemented the first time. 😅

--- app/javascript/ui/test_collections/FeedbackTermsModal.js
+++ app/javascript/ui/test_collections/FeedbackTermsModal.js
@@ -7,104 +7,25 @@ import { ModalCloseButton } from '~/ui/global/modals/Modal'
 import PaperAirplane from '~/ui/test_collections/PaperAirplane'
 import { FormButton } from '~/ui/global/styled/forms'
 import { DisplayText } from '~/ui/global/styled/typography'
-import { List, ListItem } from '~/ui/global/styled/lists'
 import Link from '~/ui/global/Link'
 import v from '~/utils/variables'
 import CloseIcon from '../icons/CloseIcon'
 
-// Extract children from here
-// Reuse for both Agree to Terms and Confirm Payment?
-const FeedbackTermsModal = ({ onSubmit, open, close }) => (
-  <StyledDialog
-    classes={{ root: 'root__dialog', paper: 'modal__paper' }}
-    open={open}
-    BackdropProps={{
-      invisible: true,
-    }}
-  >
-    <StyledModalCloseButton onClick={close}>
-      <CloseIcon />
-    </StyledModalCloseButton>
-    <DialogContent>
-      <form onSubmit={onSubmit}>
-        <PaperAirplane />
-        <SpecialDisplayHeading wrapLine>
-          Before you launch your first test, please review and agree to the
-          terms below.
-        </SpecialDisplayHeading>
-        <StyledDiv>
-          <StyledDisplayText>
-            You understand that as a human-centered business, Shape expects you
-            to behave ethically to other users and people who respond to your
-            surveys anonymously, and in accordance with the Terms of Use and
-            applicable law. You also understand that IDEO does not have any
-            control over the contents of your surveys which are intended to be
-            anonymous, but requires the following house rules:
-          </StyledDisplayText>
-        </StyledDiv>
-        <StyledDiv>
-          <List>
-            <ListItem>
-              <StyledDisplayText>
-                1. You agree not to ask for personally identifiable or sensitive
-                information from users including their name or image.{' '}
-              </StyledDisplayText>
-            </ListItem>
-            <ListItem>
-              <StyledDisplayText>
-                2. You agree that Shape is not responsible for the contents of
-                the survey, and that these are your/your organization’s
-                responsibility.
-              </StyledDisplayText>
-            </ListItem>
-            <ListItem>
-              <StyledDisplayText>
-                3. You agree not to include offensive or discriminatory language
-                or intent in the surveys or include content or style likely to
-                affect Shape’s reputation as an ethical business.
-              </StyledDisplayText>
-            </ListItem>
-            <ListItem>
-              <StyledDisplayText>
-                4. You agree that Shape may remove, modify, terminate your
-                access to surveys or the Platform if you breach the above rules,
-                in addition to its rights under the Terms of Use.
-              </StyledDisplayText>
-            </ListItem>
-          </List>
-        </StyledDiv>
-        <StyledDiv>
-          <StyledDisplayText>
-            You can find the full terms of use for Shape at{' '}
-            <Link to="/terms" target="_blank">
-              shape.space/terms
-            </Link>
-            .
-          </StyledDisplayText>
-        </StyledDiv>
-        <StyledDiv style={{ textAlign: 'center' }}>
-          <FormButton width={300}>I agree to these terms</FormButton>
-        </StyledDiv>
-      </form>
-    </DialogContent>
-  </StyledDialog>
-)
-
 const SpecialDisplayHeading = styled.p`
   margin: 0;
-  margin-top: 10px;
-  margin-bottom: 20px;
-  line-height: 1.625rem;
+  margin-top: 2rem;
+  margin-bottom: 1.25rem;
+  line-height: 30px;
   font-family: ${v.fonts.sans};
   font-size: 1.25rem;
   font-weight: ${v.weights.book};
   color: ${v.colors.black};
+  text-align: center;
 `
 
 const StyledDialog = styled(Dialog)`
   .modal__paper {
-    padding: 20px;
-    padding-top: 35px;
+    padding-top: 65px;
     max-width: 680px;
     max-height: 692px;
     width: 100%;
@@ -122,13 +43,91 @@ const StyledDisplayText = styled(DisplayText)`
   font-size: 0.75rem;
 `
 
-const StyledModalCloseButton = styled(ModalCloseButton)`
-  top: 35px;
+const StyledListItem = styled.li`
+  margin-top: 0.75rem;
 `
 
-const StyledDiv = styled.div`
-  margin: 1.5rem 0;
-`
+const FeedbackTermsModal = ({ onSubmit, open, close }) => (
+  <StyledDialog
+    classes={{ root: 'root__dialog', paper: 'modal__paper' }}
+    open={open}
+    BackdropProps={{
+      invisible: true,
+    }}
+  >
+    <ModalCloseButton onClick={close}>
+      <CloseIcon />
+    </ModalCloseButton>
+    <DialogContent>
+      <form onSubmit={onSubmit} style={{ padding: '0 29px' }}>
+        <PaperAirplane />
+        <SpecialDisplayHeading wrapLine>
+          Before you launch your first test, please review and agree to the
+          terms below.
+        </SpecialDisplayHeading>
+        <div style={{ marginTop: '1.25rem' }}>
+          <StyledDisplayText>
+            You understand that as a human-centered business, Shape expects you
+            to behave ethically to other users and people who respond to your
+            surveys anonymously, and in accordance with the Terms of Use and
+            applicable law. You also understand that IDEO does not have any
+            control over the contents of your surveys which are intended to be
+            anonymous, but requires the following house rules:
+          </StyledDisplayText>
+        </div>
+        <div>
+          <ul style={{ marginLeft: '1rem' }}>
+            <StyledListItem>
+              <StyledDisplayText>
+                1. You agree not to ask for personally identifiable or sensitive
+                information from users including their name or image.{' '}
+              </StyledDisplayText>
+            </StyledListItem>
+            <StyledListItem>
+              <StyledDisplayText>
+                2. You agree that Shape is not responsible for the contents of
+                the survey, and that these are your/your organization’s
+                responsibility.
+              </StyledDisplayText>
+            </StyledListItem>
+            <StyledListItem>
+              <StyledDisplayText>
+                3. You agree not to include offensive or discriminatory language
+                or intent in the surveys or include content or style likely to
+                affect Shape’s reputation as an ethical business.
+              </StyledDisplayText>
+            </StyledListItem>
+            <StyledListItem>
+              <StyledDisplayText>
+                4. You agree that Shape may remove, modify, terminate your
+                access to surveys or the Platform if you breach the above rules,
+                in addition to its rights under the Terms of Use.
+              </StyledDisplayText>
+            </StyledListItem>
+          </ul>
+        </div>
+        <div
+          style={{
+            textAlign: 'center',
+            marginTop: '12px',
+            marginBottom: '22px',
+          }}
+        >
+          <StyledDisplayText>
+            You can find the full terms of use for Shape at{' '}
+            <Link to="/terms" target="_blank">
+              shape.space/terms
+            </Link>
+            .
+          </StyledDisplayText>
+        </div>
+        <div style={{ textAlign: 'center', marginBottom: '4px' }}>
+          <FormButton width={300}>I agree to these terms</FormButton>
+        </div>
+      </form>
+    </DialogContent>
+  </StyledDialog>
+)
 
 FeedbackTermsModal.propTypes = {
   open: PropTypes.bool.isRequired,
