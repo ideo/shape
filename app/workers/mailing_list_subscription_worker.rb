092f3498aa88807706938e6798cd5ba98312f3f1
kaptron <dkaplan@ideo.com>
2019-06-11 16:27:05 -0700

    Disable MailingListSubscriptionWorker in dev (e.g. cypress)

--- app/workers/mailing_list_subscription_worker.rb
+++ app/workers/mailing_list_subscription_worker.rb
@@ -2,6 +2,9 @@ class MailingListSubscriptionWorker
   include Sidekiq::Worker
 
   def perform(user_id, list, subscribe)
+    # only run in test/production environments
+    return if Rails.env.development?
+
     user = User.find(user_id)
 
     MailingListSubscription.call(
@@ -9,5 +12,8 @@ class MailingListSubscriptionWorker
       list: list,
       subscribe: subscribe,
     )
+  rescue ActiveRecord::RecordNotFound
+    # likely in dev / cypress task where the user was already destroyed
+    logger.debug 'MailingListSubscriptionWorker: user not found.'
   end
 end
