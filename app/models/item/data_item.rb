2d2f62a4d1f8b883fdbdabf4b024d1589840d733
Nicu Listana <nlistana@ideo.com>
2019-06-12 13:41:20 -0700

    legend card items on position 3 by on inserts by default

--- app/models/item/data_item.rb
+++ app/models/item/data_item.rb
@@ -85,7 +85,7 @@ class Item
       parent_collection ||= parent
       builder = CollectionCardBuilder.new(
         params: {
-          order: parent_collection_card.order + 1,
+          order: 3, # default legend position on inserts
           width: 1,
           height: 2,
           item_attributes: {
