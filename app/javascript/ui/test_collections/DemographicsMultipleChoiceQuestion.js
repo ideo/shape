2f93f8039224c79ab7799cab246d280ab8ea9f4b
John Jensen <john.jensen@substantial.com>
2019-06-25 17:26:58 -0700

    Add DemographicsQuestionHolder to share layout

--- app/javascript/ui/test_collections/DemographicsMultipleChoiceQuestion.js
+++ app/javascript/ui/test_collections/DemographicsMultipleChoiceQuestion.js
@@ -1,55 +1,14 @@
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
-import { observer, PropTypes as MobxPropTypes } from 'mobx-react'
-import { FormControlLabel, Checkbox, FormGroup } from '@material-ui/core'
+import { PropTypes as MobxPropTypes } from 'mobx-react'
+import { Checkbox, FormGroup } from '@material-ui/core'
 import { CheckCircle, RadioButtonUnchecked } from '@material-ui/icons'
 
-import { SmallHelperText, DisplayTextCss } from '~/ui/global/styled/typography'
-import v from '~/utils/variables'
-import { validDemographicsCategories } from '~/ui/test_collections/RespondentDemographics'
 import {
-  QuestionText,
-  QuestionSpacingContainer,
-} from '~/ui/test_collections/shared'
+  DemographicsQuestionHolder,
+  QuestionShape,
+  StyledFormControlLabel,
+} from './DemographicsQuestionHolder'
 
-// TODO duplication
-const Scale = styled.div`
-  background-color: ${props => props.theme.responseHolder};
-  box-sizing: border-box;
-  padding: 7px 13px;
-  width: 100%;
-`
-
-const StyledFormControlLabel = styled(FormControlLabel)`
-  .label {
-    ${DisplayTextCss};
-    color: ${v.colors.primaryDark};
-  }
-`
-
-const StyledRoundChevronRight = styled.span`
-  height: 30px;
-  width: 30px;
-  color: ${v.colors.white};
-  background-color: ${v.colors.primaryDark};
-  border-radius: 50%;
-  display: flex;
-  flex-direction: column;
-  align-content: center;
-  justify-content: center;
-  font-size: 1.6rem;
-`
-
-const SkipButton = ({ onClick }) => (
-  <button onClick={onClick}>
-    <StyledRoundChevronRight>&#62;</StyledRoundChevronRight>
-  </button>
-)
-SkipButton.propTypes = {
-  onClick: PropTypes.func.isRequired,
-}
-
-@observer
 class DemographicsMultipleChoiceQuestion extends React.Component {
   state = {
     selections: [],
@@ -95,68 +54,38 @@ class DemographicsMultipleChoiceQuestion extends React.Component {
   }
 
   render() {
-    const {
-      question: { prompt, choices },
-    } = this.props
+    const { question } = this.props
 
     return (
-      <div style={{ width: '100%' }}>
-        <QuestionSpacingContainer>
-          <QuestionText>
-            {prompt}
-            <SmallHelperText
-              color={v.colors.demographicsHint}
-              style={{ display: 'block' }}
-            >
-              Please select all that apply. This question is optional.
-            </SmallHelperText>
-          </QuestionText>
-        </QuestionSpacingContainer>
-        <Scale>
-          <FormGroup
-            style={{
-              paddingTop: '1rem',
-              paddingBottom: '1rem',
-            }}
-          >
-            {choices.map((choice, index) => (
-              <StyledFormControlLabel
-                key={index}
-                classes={{ label: 'label' }}
-                label={choice.text}
-                labelPlacement="end"
-                control={
-                  <Checkbox
-                    checkedIcon={<CheckCircle />}
-                    icon={<RadioButtonUnchecked />}
-                    onChange={(_e, checked) =>
-                      this.handleAnswer(index, checked)
-                    }
-                  />
-                }
-              />
-            ))}
-          </FormGroup>
-          <div style={{ textAlign: 'right' }}>
-            <SkipButton onClick={_e => this.showNextQuestion()} />
-          </div>
-        </Scale>
-      </div>
+      <DemographicsQuestionHolder
+        instructions="Please select all that apply. This question is optional."
+        question={question}
+        onNextQuestion={() => this.showNextQuestion()}
+      >
+        <FormGroup>
+          {question.choices.map((choice, index) => (
+            <StyledFormControlLabel
+              key={index}
+              classes={{ label: 'label' }}
+              label={choice.text}
+              labelPlacement="end"
+              control={
+                <Checkbox
+                  checkedIcon={<CheckCircle />}
+                  icon={<RadioButtonUnchecked />}
+                  onChange={(_e, checked) => this.handleAnswer(index, checked)}
+                />
+              }
+            />
+          ))}
+        </FormGroup>
+      </DemographicsQuestionHolder>
     )
   }
 }
 
 DemographicsMultipleChoiceQuestion.propTypes = {
-  question: PropTypes.shape({
-    prompt: PropTypes.string.isRequired,
-    category: PropTypes.oneOf(validDemographicsCategories()).isRequired,
-    choices: PropTypes.arrayOf(
-      PropTypes.shape({
-        text: PropTypes.string.isRequired,
-        tags: PropTypes.arrayOf(PropTypes.string).isRequired,
-      })
-    ),
-  }).isRequired,
+  question: QuestionShape.isRequired,
   user: MobxPropTypes.objectOrObservableObject,
   onAnswer: PropTypes.func.isRequired,
 }
--- app/javascript/ui/test_collections/DemographicsSingleChoiceQuestion.js
+++ app/javascript/ui/test_collections/DemographicsSingleChoiceQuestion.js
@@ -1,55 +1,14 @@
 import PropTypes from 'prop-types'
-import styled from 'styled-components'
-import { observer, PropTypes as MobxPropTypes } from 'mobx-react'
-import { FormControlLabel, Radio, RadioGroup } from '@material-ui/core'
+import { PropTypes as MobxPropTypes } from 'mobx-react'
+import { Radio, RadioGroup } from '@material-ui/core'
 import { CheckCircle, RadioButtonUnchecked } from '@material-ui/icons'
 
-import { SmallHelperText, DisplayTextCss } from '~/ui/global/styled/typography'
-import v from '~/utils/variables'
-import { validDemographicsCategories } from '~/ui/test_collections/RespondentDemographics'
 import {
-  QuestionText,
-  QuestionSpacingContainer,
-} from '~/ui/test_collections/shared'
+  DemographicsQuestionHolder,
+  QuestionShape,
+  StyledFormControlLabel,
+} from './DemographicsQuestionHolder'
 
-// TODO duplication
-const Scale = styled.div`
-  background-color: ${props => props.theme.responseHolder};
-  box-sizing: border-box;
-  padding: 7px 13px;
-  width: 100%;
-`
-
-const StyledFormControlLabel = styled(FormControlLabel)`
-  .label {
-    ${DisplayTextCss};
-    color: ${v.colors.primaryDark};
-  }
-`
-
-const StyledRoundChevronRight = styled.span`
-  height: 30px;
-  width: 30px;
-  color: ${v.colors.white};
-  background-color: ${v.colors.primaryDark};
-  border-radius: 50%;
-  display: flex;
-  flex-direction: column;
-  align-content: center;
-  justify-content: center;
-  font-size: 1.6rem;
-`
-
-const SkipButton = ({ onClick }) => (
-  <button onClick={onClick}>
-    <StyledRoundChevronRight>&#62;</StyledRoundChevronRight>
-  </button>
-)
-SkipButton.propTypes = {
-  onClick: PropTypes.func.isRequired,
-}
-
-@observer
 class DemographicsSingleChoiceQuestion extends React.Component {
   state = {
     selectedChoice: null,
@@ -83,68 +42,41 @@ class DemographicsSingleChoiceQuestion extends React.Component {
   }
 
   render() {
-    const {
-      question: { prompt, choices },
-    } = this.props
+    const { question } = this.props
 
     return (
-      <div style={{ width: '100%' }}>
-        <QuestionSpacingContainer>
-          <QuestionText>
-            {prompt}
-            <SmallHelperText
-              color={v.colors.demographicsHint}
-              style={{ display: 'block' }}
-            >
-              This question is optional.
-            </SmallHelperText>
-          </QuestionText>
-        </QuestionSpacingContainer>
-        <Scale>
-          <RadioGroup
-            value={this.state.selectedChoice}
-            onChange={(_e, value) => this.handleAnswer(value)}
-            style={{
-              paddingTop: '1rem',
-              paddingBottom: '1rem',
-            }}
-          >
-            {choices.map((choice, index) => (
-              <StyledFormControlLabel
-                key={index}
-                value={index.toString()}
-                classes={{ label: 'label' }}
-                label={choice.text}
-                labelPlacement="end"
-                control={
-                  <Radio
-                    checkedIcon={<CheckCircle />}
-                    icon={<RadioButtonUnchecked />}
-                  />
-                }
-              />
-            ))}
-          </RadioGroup>
-          <div style={{ textAlign: 'right' }}>
-            <SkipButton onClick={_e => this.showNextQuestion()} />
-          </div>
-        </Scale>
-      </div>
+      <DemographicsQuestionHolder
+        instructions="This question is optional."
+        question={question}
+        onNextQuestion={() => this.showNextQuestion()}
+      >
+        <RadioGroup
+          value={this.state.selectedChoice}
+          onChange={(_e, value) => this.handleAnswer(value)}
+        >
+          {question.choices.map((choice, index) => (
+            <StyledFormControlLabel
+              key={index}
+              value={index.toString()}
+              classes={{ label: 'label' }}
+              label={choice.text}
+              labelPlacement="end"
+              control={
+                <Radio
+                  checkedIcon={<CheckCircle />}
+                  icon={<RadioButtonUnchecked />}
+                />
+              }
+            />
+          ))}
+        </RadioGroup>
+      </DemographicsQuestionHolder>
     )
   }
 }
 
 DemographicsSingleChoiceQuestion.propTypes = {
-  question: PropTypes.shape({
-    prompt: PropTypes.string.isRequired,
-    category: PropTypes.oneOf(validDemographicsCategories()).isRequired,
-    choices: PropTypes.arrayOf(
-      PropTypes.shape({
-        text: PropTypes.string.isRequired,
-        tags: PropTypes.arrayOf(PropTypes.string).isRequired,
-      })
-    ),
-  }).isRequired,
+  question: QuestionShape.isRequired,
   user: MobxPropTypes.objectOrObservableObject,
   onAnswer: PropTypes.func.isRequired,
 }
