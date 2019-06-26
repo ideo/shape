511e0342db486d96aedc7681f1fe646154f6b39a
Beau Dacious <dacious.beau@gmail.com>
2019-06-26 11:05:10 -0700

    remove duplication from demographics intro

--- app/javascript/ui/test_collections/DemographicsIntroQuestion.js
+++ app/javascript/ui/test_collections/DemographicsIntroQuestion.js
@@ -1,53 +1,34 @@
-import styled from 'styled-components'
-
-import { DisplayText } from '~/ui/global/styled/typography'
 import Emoji from '~/ui/icons/Emoji'
-import v from '~/utils/variables'
-
-// TODO duplication
-const Question = styled.div`
-  border-color: ${props => props.theme.borderColor};
-  border-bottom-style: solid;
-  border-bottom-width: 4px;
-  box-sizing: border-box;
-  color: white;
-  padding: 12px 12px 16px 12px;
-  width: 100%;
-  .editable-text {
-    margin: -1px -1px -1px 5px;
-    padding: 2px 3px;
-    transition: background-color 250ms;
-    display: inline-block;
-  }
-  &:hover .editable-text {
-    background-color: rgba(255, 255, 255, 0.5);
-  }
-`
-Question.displayName = 'Question'
+import {
+  QuestionText,
+  QuestionSpacingContainer,
+} from '~/ui/test_collections/shared'
+import { EmojiHolder } from '~/ui/test_collections/ScaleQuestion'
 
 class DemographicsIntroQuestion extends React.Component {
   render() {
     return (
       <div style={{ width: '100%' }}>
-        <Question
-          editing={false}
+        <QuestionSpacingContainer
           style={{
             display: 'flex',
             flexDirection: 'column',
             alignItems: 'center',
           }}
         >
-          <Emoji
-            name="A telescope mounted on a tripod."
-            symbol="🔭"
-            scale={1.5}
-          />
-          <DisplayText color={v.colors.white}>
+          <EmojiHolder data-cy="RecontactEmojiHolder">
+            <Emoji
+              name="A telescope mounted on a tripod."
+              symbol="🔭"
+              scale={1.5}
+            />
+          </EmojiHolder>
+          <QuestionText>
             Please answer a few questions to help us direct you to the next
             survey. The more you answer, the more opportunities we can find for
             you.
-          </DisplayText>
-        </Question>
+          </QuestionText>
+        </QuestionSpacingContainer>
       </div>
     )
   }
