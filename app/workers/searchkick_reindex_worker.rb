dd1e6d27f10b9a057d3be856aed9b1139756187d
kaptron <dkaplan@ideo.com>
2019-07-17 14:43:05 -0700

    update searchkick_reindex_worker_spec to reflect the addition of Items

--- app/workers/searchkick_reindex_worker.rb
+++ app/workers/searchkick_reindex_worker.rb
@@ -4,10 +4,10 @@ class SearchkickReindexWorker
   include Sidekiq::Worker
 
   def perform
-    # Index any collections that were queued
-    puts ':: Queueing Item / Collection reindex ::'
-    puts "Collections: #{Collection.search_index.reindex_queue.length}"
-    puts "Items: #{Item.search_index.reindex_queue.length}"
+    # Pick up items/collections that were queued for async reindexing
+    logger.debug '-- Queueing Item / Collection reindex --'
+    logger.debug "Collections: #{Collection.search_index.reindex_queue.length}"
+    logger.debug "Items: #{Item.search_index.reindex_queue.length}"
     Searchkick::ProcessQueueJob.perform_later(class_name: 'Collection')
     Searchkick::ProcessQueueJob.perform_later(class_name: 'Item')
   end
