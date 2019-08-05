ae011e47595291595f78bd4fb4e8c1dffec3e15f
mtvillwock <mtvillwock@gmail.com>
2019-08-05 15:07:13 -0700

    Make title and cover editable for most objects
    **What**
    Allow users to edit title and cover for these items:
    - links
    - videos
    - collections
    - foamcore boards
    - feedback (test collections)
    - submission boxes
    
    **Why**
    Give users superpowers!
    
    Tickets/Trello Cards:
    https://trello.com/c/Qe3FOfDU/1139-3-edit-form-for-titles-and-covers-of-most-objects

---
+++ app/javascript/ui/icons/EditPencilIconLarge.js
@@ -0,0 +1,14 @@
+import Icon from './Icon'
+
+const EditPencilIconLarge = () => (
+  <Icon fill>
+    <svg version="1.1" x="0px" y="0px"
+    viewBox="0 0 32 32" style="enable-background:new 0 0 32 32;">
+      <path d="M28.8,6.8l-3.5-3.5c-0.4-0.4-1-0.4-1.5,0.1L3.9,23.3L3,27.7c-0.1,0.3,0,0.6,0.2,0.9c0.2,0.2,0.4,0.3,0.7,0.3
+      c0.1,0,0.1,0,0.2,0l4.7-0.7L28.7,8.3C28.9,8.1,29,7.8,29,7.6C29.1,7.3,29,7,28.8,6.8z M8.2,26.9l-3.8,0.6l0.7-3.6L21.2,7.8l3.1,3.1
+      L8.2,26.9z M25.2,9.9l-3.1-3.1l2.4-2.4l3.1,3.1L25.2,9.9z"/>
+    </svg>
+  </Icon>
+)
+
+export EditPencilIconLarge
