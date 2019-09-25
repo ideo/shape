0cbb0296a9f6560a45c1c80919e8dea38705a61d
mtvillwock <mtvillwock@gmail.com>
2019-09-25 11:44:33 -0700

    Fix schema issue with GroupHierarchy migration
    **What**
    Delete and recreate migration file.
    
    **Why**
    Was getting codeship error after merging in development
    https://app.codeship.com/projects/273385/builds/43948561?pipeline=2432d108-50ca-4846-b624-c46287ad71bb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_10_190757) do
+ActiveRecord::Schema.define(version: 2019_09_25_184346) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
