1bfd41cb0868d112d04fa065d0f3685e4fc77942
Nicu Listana <nlistana@ideo.com>
2019-10-08 15:06:14 -0700

    persist unhighlight data_content state

--- app/javascript/stores/UiStore.js
+++ app/javascript/stores/UiStore.js
@@ -907,15 +907,19 @@ export default class UiStore {
     const { selectedTextRangeForCard, currentQuillEditor } = this
     const { range } = selectedTextRangeForCard
     if (currentQuillEditor && range && range.length) {
-      // NOTE: This sort of works... however it also needs to save the text item
-      // otherwise the removal of the highlight won't persist
-      currentQuillEditor.formatText(
+      const delta = currentQuillEditor.formatText(
         range.index,
         range.length,
         'commentHighlight',
         false,
         'user'
       )
+      // for some reason currentQuillEditor.editor's get methods are still the old version
+      // doing so below to force the record's data_content to reflect the updates that we want
+      this.commentingOnRecord.data_content.ops = delta.ops
+      this.commentingOnRecord.data_content.version =
+        this.commentingOnRecord.data_content.version + 1
+      this.commentingOnRecord.save()
     }
     this.selectedTextRangeForCard = { ...this.defaultSelectedTextRange }
   }
