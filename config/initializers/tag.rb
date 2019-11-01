b57105fc96674a8c85985591133dd8e0e03ff4a3
kaptron <dkaplan@ideo.com>
2019-11-01 11:04:10 -0700

    Fix searching for tags with dashes

--- config/initializers/tag.rb
+++ config/initializers/tag.rb
@@ -1,13 +1,20 @@
+ActsAsTaggableOn.remove_unused_tags = true
+ActsAsTaggableOn.strict_case_match = false
+ActsAsTaggableOn.force_lowercase = false
+# ActsAsTaggableOn.force_parameterize = true
+
 # Adds an after_save callback for the Tagging class to add our custom
 # `organization_id` to the Tag
-class ActsAsTaggableOn::Tagging
-  after_save do
-    if taggable.respond_to?(:organization_id)
-      organization_id = taggable.organization_id
+module ActsAsTaggableOn
+  class Tagging
+    after_save do
+      if taggable.respond_to?(:organization_id)
+        organization_id = taggable.organization_id
 
-      unless tag.organization_ids.include?(organization_id)
-        tag.organization_ids << organization_id
-        tag.save
+        unless tag.organization_ids.include?(organization_id)
+          tag.organization_ids << organization_id
+          tag.save
+        end
       end
     end
   end
