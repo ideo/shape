36b620c4091baa0ad9a46fd994717c749c0240c5
Nicu Listana <nlistana@ideo.com>
2019-12-13 10:06:05 -0800

    remove todo statement

--- app/workers/update_template_instances_worker.rb
+++ app/workers/update_template_instances_worker.rb
@@ -1,8 +1,6 @@
 class UpdateTemplateInstancesWorker
   include Sidekiq::Worker
 
-  # TODO: add sidekiq rate limiter to throttle instance updates
-
   def perform(master_template_id)
     template = Collection.find(master_template_id)
     template.update_template_instances if template.present?
