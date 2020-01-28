20d6ab92fc4e70e51208c1fa9ebd3a45324434b0
Marco Segreto <msegreto@ideo.com>
2020-01-28 13:29:47 -0800

    Move org destroying to end in refresher
    Otherwise there might be a small amount of time where there is no
    shell org for use.

--- app/workers/organization_shell_refresh_worker.rb
+++ app/workers/organization_shell_refresh_worker.rb
@@ -2,9 +2,10 @@ class OrganizationShellRefreshWorker
   include Sidekiq::Worker
 
   def perform
-    Organization.where(shell: true).destroy_all
+    old_organizations = Organization.where(shell: true)
     [0.10].each do
       OrganizationShellBuilder.new.save
     end
+    older_organizations.destroy_all
   end
 end
