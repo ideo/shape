136a63928c21f1fc289a5453ee26fdad066d08e0
kaptron <dkaplan@ideo.com>
2020-01-29 13:52:45 -0800

    Adds new template route for creating an instance and redirecting to it

---
+++ spec/requests/templates_controller_spec.rb
@@ -0,0 +1,70 @@
+require 'rails_helper'
+
+describe TemplatesController, type: :request, auth: true do
+  include ApplicationHelper
+  let(:user) { @user }
+
+  describe '#use_in_my_collection' do
+    let(:collection) { create(:collection, master_template: true) }
+    let(:path) { "/templates/#{collection.id}/use_in_my_collection" }
+
+    context 'not logged in', auth: false do
+      it 'should redirect to login' do
+        expect(get(path)).to redirect_to(login_url)
+      end
+    end
+
+    context 'without view access' do
+      it 'should redirect to root path' do
+        expect(get(path)).to redirect_to(root_url)
+      end
+    end
+
+    context 'with non-template' do
+      let(:collection) { create(:collection, add_viewers: [user]) }
+
+      it 'should redirect to root path' do
+        expect(get(path)).to redirect_to(root_url)
+      end
+    end
+
+    xcontext 'without an organization' do
+      let(:collection) { create(:collection, master_template: true, add_viewers: [user]) }
+
+      it 'should redirect to root path AND DO SOMETHING ELSE' do
+        expect(get(path)).to redirect_to(root_url)
+      end
+    end
+
+    context 'with an organization' do
+      let(:organization) { create(:organization) }
+      let(:builder_double) { double('CollectionTemplateBuilder') }
+      let(:instance) { create(:collection) }
+
+      before do
+        # add user to org
+        user.add_role(Role::MEMBER, organization.primary_group)
+        organization.setup_user_membership_and_collections(user)
+
+        allow(CollectionTemplateBuilder).to receive(:new).and_return(builder_double)
+        allow(builder_double).to receive(:call).and_return(true)
+        allow(builder_double).to receive(:collection).and_return(instance)
+      end
+
+      context 'with a common viewable template' do
+        let(:collection) { create(:collection, master_template: true, common_viewable: true) }
+
+        it 'should call CollectionTemplateBuilder' do
+          expect(CollectionTemplateBuilder).to receive(:new).with(
+            parent: user.current_user_collection,
+            template: collection,
+            placement: 'end',
+            created_by: user,
+          )
+          expect(builder_double).to receive(:call)
+          expect(get(path)).to redirect_to(frontend_url_for(instance))
+        end
+      end
+    end
+  end
+end
