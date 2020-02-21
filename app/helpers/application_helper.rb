b68b5fd6c47c8092f7ce102dbb44501ef7fdb4e5
kaptron <dkaplan@ideo.com>
2020-02-20 16:49:46 -0800

    [hotfix] NotificationMailer errors when no activity target is found

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,26 +1,14 @@
 module ApplicationHelper
   # NOTE: this is somewhat similar to RoutingStore.js by necessity
-  def frontend_url_for(obj)
+  def frontend_url_for(obj, with_id: true)
     return admin_root_url if obj == Role::SHAPE_ADMIN.to_s.titleize
 
-    url = "#{root_url}#{obj.organization.slug}/"
+    url = "#{root_url}#{obj&.organization&.slug}/"
+    obj_id = with_id ? "/#{obj.id}" : ''
     if obj.is_a? Collection
-      url += "collections/#{obj.id}"
+      url += "collections#{obj_id}"
     elsif obj.is_a? Item
-      url += "items/#{obj.id}"
-    end
-    url
-  end
-
-  def frontend_url_for_type(obj)
-    return admin_root_url if obj == Role::SHAPE_ADMIN.to_s.titleize
-
-    url = "#{root_url}#{obj.organization.slug}/"
-
-    if obj.is_a? Collection
-      "#{url}collections"
-    elsif obj.is_a? Item
-      "#{url}items"
+      url += "items/#{obj_id}"
     end
     url
   end
