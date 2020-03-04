2dd10090cdadaf2c5ebd5bfe3dac5a488f9147eb
niculistana <niculistana@gmail.com>
2020-03-04 13:41:27 -0800

    fix `frontend_path_for` ApplicationHelper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,8 @@ module ApplicationHelper
 
   def frontend_path_for(obj, with_id: true)
     slug = obj&.organization&.slug
-    url = "/#{slug}" if slug
+    url = '/'
+    url += slug.to_s if slug
     obj_id = with_id ? "/#{obj.id}" : ''
     if obj.is_a? Collection
       url += "/collections#{obj_id}"
