a1f35a302ff86e39921c56cb2a2036c4a2ed6f54
Josh Schwartzman <josh@dreamseedo.org>
2020-03-18 13:22:08 -0700

    Add test for multiple filters

--- app/services/search.rb
+++ app/services/search.rb
@@ -1,22 +1,34 @@
 class Search
+  attr_reader :options
+
+  DEFAULT_OPTIONS = {
+    fields: [
+      'name^50',
+      'handle^50',
+      'tags^20',
+      'content',
+    ],
+    # `missing` just means this field is allowed to be missing
+    boost_by: { activity_count: { factor: 0.2, missing: 0 } },
+    per_page: 10,
+    page: 1,
+    where: {},
+  }.freeze
+
   def initialize(options = {})
-    default_options = {
-      fields: [
-        'name^50',
-        'handle^50',
-        'tags^20',
-        'content',
-      ],
-      # `missing` just means this field is allowed to be missing
-      boost_by: { activity_count: { factor: 0.2, missing: 0 } },
-      per_page: 10,
-      page: 1,
-      where: {},
-    }
-    @options = default_options.merge(options)
+    @options = DEFAULT_OPTIONS.merge(options)
   end
 
   def search(query)
+    modified_query = apply_filters(query)
+
+    Searchkick.search(
+      modified_query.presence || '*',
+      @options,
+    )
+  end
+
+  def apply_filters(query)
     filters.each do |filter|
       f = filter.new(query)
       next unless f.match?
@@ -24,11 +36,7 @@ class Search
       @options = @options.deep_merge(f.options)
       query = f.modify_query
     end
-
-    Searchkick.search(
-      query.blank? ? '*' : query,
-      @options,
-    )
+    query
   end
 
   private
