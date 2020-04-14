37566b3a68b46ff142711ab47981d5d88dde986a
niculistana <niculistana@gmail.com>
2020-04-13 18:07:35 -0700

    add TemplateInstanceCard::TemplateInstanceTextCardUpdater spec

--- app/services/template_instance_card/template_instance_text_card_updater.rb
+++ app/services/template_instance_card/template_instance_text_card_updater.rb
@@ -14,7 +14,7 @@ module TemplateInstanceCard
 
     def copy_data_content_from_master!
       master_data_content = Mashie.new(@master_card.item.data_content)
-      return if @instance_card.item.version.to_i > @master_card.item.version.to_i
+      return if @instance_card.item.version.to_i > @master_card.item.version.to_i || @instance_card.item.activities.where_participated.any?
 
       @instance_card.item.update(data_content: Mashie.new(ops: master_data_content.ops, version: master_data_content.version))
     end
