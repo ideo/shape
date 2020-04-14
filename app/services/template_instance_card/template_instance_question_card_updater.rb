854384aaabf20d278fee8df7492ec5d71ff66d3b
niculistana <niculistana@gmail.com>
2020-04-13 18:37:49 -0700

    add TemplateInstanceCard::TemplateInstanceQuestionCardUpdater rspec tests

--- app/services/template_instance_card/template_instance_question_card_updater.rb
+++ app/services/template_instance_card/template_instance_question_card_updater.rb
@@ -13,8 +13,10 @@ module TemplateInstanceCard
 
     def copy_test_details_from_master!
       # copy more details over if we are still setting up our submission template test
+
       test = @instance_card.parent
       return unless test.is_a?(Collection::TestCollection) && test.draft?
+
       # Skip if we reach the ideas collection
       return if @master_card.record.is_a?(Collection)
 
