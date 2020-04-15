b21c23e72c538eb5d199e80c900f5855a1185210
Marco Segreto <msegreto@ideo.com>
2020-04-15 14:43:40 -0700

    Adds creation of channel collection

--- app/services/slack/app_mentioned.rb
+++ app/services/slack/app_mentioned.rb
@@ -2,16 +2,26 @@ module Slack
   class AppMentioned
     include Interactor
 
-    delegate_to_context :event
+    delegate_to_context :event, :collection
     require_in_context :event
     delegate :channel, :message_ts, :links, to: :event
 
     def call
       return unless run?
 
+      context.collection = find_or_create_channel_search_collection
+
+      # TODO read last 3 months of slack messages, find links, create link
+      # collections for each link
     end
 
     private
+
+    def find_or_create_channel_search_collection
+      Slack::InShape::CreateChannelSearchCollection.call(
+        channel: channel
+      )
+    end
 
     def run?
       event.type == 'app_mention'
