2c5a2dcbe99fc4953ea990d0bd453bf99fb45add
niculistana <niculistana@gmail.com>
2020-04-30 11:27:18 -0700

    fix styleguidist build error - Adds sso sdk config

--- app/javascript/utils/IdeoSSO.js
+++ app/javascript/utils/IdeoSSO.js
@@ -1,5 +1,7 @@
 import IdeoSSO from 'ideo-sso-js-sdk'
 
+window.CONFIG = window.CONFIG || {}
+
 IdeoSSO.initFromEnv({
   IDEO_SSO_HOST: window.CONFIG.IDEO_SSO_HOST,
   IDEO_SSO_CLIENT_ID: window.CONFIG.IDEO_SSO_CLIENT_ID,
