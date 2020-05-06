36a2dad946da5ec95d4b06ba08f9f83f52f24019
mtvillwock <mtvillwock@gmail.com>
2020-05-06 12:36:55 -0700

    WIP: convert C∆ Tabs to observer

--- app/javascript/ui/org_settings/CreativeDifferenceTabs.js
+++ app/javascript/ui/org_settings/CreativeDifferenceTabs.js
@@ -1,5 +1,4 @@
 import PropTypes from 'prop-types'
-import { Fragment, useState, useEffect } from 'react'
 // import { makeStyles } from '@material-ui/core/styles'
 import AppBar from '@material-ui/core/AppBar'
 import Tabs from '@material-ui/core/Tabs'
@@ -15,6 +14,8 @@ import {
 } from 'c-delta-organization-settings'
 import { routingStore } from '~/stores'
 import Loader from '~/ui/layout/Loader'
+import { runInAction, observable, action } from 'mobx'
+import { observer } from 'mobx-react'
 
 function TabPanel(props) {
   const { children, value, tabName } = props
@@ -53,128 +54,118 @@ function a11yProps(index) {
 //   },
 // })
 
-// TODO: replace with JWT token data
-// const organization = { name: 'IDEO', id: 4, industry_subcategory_id: null }
-// TODO: replace orgName with org you fetch in this component
-const CreativeDifferenceTabs = ({ orgName }) => {
-  // const classes = useStyles()
-  const [value, setValue] = useState('organization')
-  const [contentVersions, setContentVersions] = useState([])
-  const [subcategories, setSubcategories] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
-  const [isError, setIsError] = useState(false)
+@observer
+class CreativeDifferenceTabs extends React.Component {
+  @observable
+  industrySubcategories = []
+  @observable
+  contentVersions = []
+  @observable
+  organization = {}
+  @observable
+  isLoading = false
+  @observable
+  isError = false
+  @observable
+  tabValue = ''
 
-  const handleChange = (event, newValue) => {
+  constructor(props) {
+    super(props)
+
+    runInAction(() => {
+      this.tabValue = props.tab || 'organization'
+    })
+  }
+
+  async componentDidMount() {
+    // fetch data here
+    const responses = await Promise.all([
+      industrySubcategoriesStore.fetch(),
+      contentVersionsStore.fetch(),
+    ])
+
+    runInAction(() => {
+      this.industrySubcategories = responses[0]
+      this.contentVersions = responses[1]
+    })
+  }
+
+  @action
+  setTabValue(val) {
+    this.tabValue = val
+  }
+
+  handleChange = (event, newValue) => {
     // event.preventDefault()
     console.log(event, newValue)
-    setValue(newValue)
+    this.setTabValue(newValue)
     routingStore.goToPath(`/org-settings/${newValue}`)
   }
 
-  // useEffect(() => {
-  //   const fetchJwtData = async () => {
-  // try {
-  // setIsLoading(true)
-  // const response = await someApiCall()
-  // const orgInfo = decrypt(response).organization
-  // setOrganization(orgInfo)
-  // setIsLoading(false)
-  // } catch (err) {
-  //   setIsError(true)
-  // send error to Sentry?
-  // }
-  //   }
-  //   fetchJwtData()
-  // }, [])
-
-  useEffect(() => {
-    async function loadIndustrySubcategories() {
-      try {
-        setIsLoading(true)
-        const result = await industrySubcategoriesStore.fetch()
-        console.log('subcategories fetch: ', result)
-        setSubcategories(result)
-        setIsLoading(false)
-      } catch (err) {
-        console.log('subcategory request failed')
-        setIsError(err)
-      }
-    }
-
-    loadIndustrySubcategories()
-  }, [])
-
-  useEffect(() => {
-    async function getContentVersions() {
-      try {
-        setIsLoading(true)
-        console.log('Contentversionstore: ', contentVersionsStore)
-        const result = await contentVersionsStore.fetch()
-        console.log('contentversions fetch: ', result)
-        setContentVersions(result)
-        setIsLoading(false)
-      } catch (err) {
-        console.log('content version request failed: ', err)
-        setIsError(err)
-      }
-    }
-
-    getContentVersions()
-  }, [])
-
-  return (
-    <div
-      style={{
-        height: '1000px',
-        // background:
-        //   'url(https://www.startrek.com/sites/default/files/styles/content_full/public/images/2019-07/c82b013313066e0702d58dc70db033ca.jpg?itok=9-M5ggoe)',
-      }}
-    >
-      {isError && <div>Something went wrong... </div>}
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <Fragment>
-          <AppBar
-            position="static"
-            style={{
-              flexGrow: 1,
-              backgroundColor: v.colors.cDeltaBlue,
-              color: v.colors.black,
-            }}
-          >
-            <Tabs
-              value={value}
-              onChange={handleChange}
-              aria-label="simple tabs example"
+  render() {
+    const { orgName, tab } = this.props
+    const {
+      industrySubcategories,
+      contentVersions,
+      isError,
+      isLoading,
+      tabValue,
+      handleChange,
+    } = this
+    return (
+      <div
+        style={{
+          height: '1000px',
+          // background:
+          //   'url(https://www.startrek.com/sites/default/files/styles/content_full/public/images/2019-07/c82b013313066e0702d58dc70db033ca.jpg?itok=9-M5ggoe)',
+        }}
+      >
+        {isError && <div>Something went wrong... </div>}
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <React.Fragment>
+            <AppBar
+              position="static"
+              style={{
+                flexGrow: 1,
+                backgroundColor: v.colors.cDeltaBlue,
+                color: v.colors.black,
+              }}
             >
-              {/* TODO: How to inject icon into this? CSS before content? */}
-              {/* TODO: Change underline for selected tab from Material-UI default */}
-              <Tab
-                value={'organization'}
-                label={`C∆ ${orgName} Settings`}
-                {...a11yProps(0)}
+              <Tabs
+                value={tab}
+                onChange={handleChange}
+                aria-label="simple tabs example"
+              >
+                {/* TODO: How to inject icon into this? CSS before content? */}
+                {/* TODO: Change underline for selected tab from Material-UI default */}
+                <Tab
+                  value={'organization'}
+                  label={`C∆ ${orgName} Settings`}
+                  {...a11yProps(0)}
+                />
+                <Tab
+                  value={'teams'}
+                  label={`C∆ ${orgName} Teams`}
+                  {...a11yProps(1)}
+                />
+              </Tabs>
+            </AppBar>
+            <TabPanel value={tabValue} tabName="organization">
+              <OrganizationTab
+                industrySubcategories={industrySubcategories}
+                contentVersions={contentVersions}
               />
-              <Tab
-                value={'teams'}
-                label={`C∆ ${orgName} Teams`}
-                {...a11yProps(1)}
-              />
-            </Tabs>
-          </AppBar>
-          <TabPanel value={value} tabName="organization">
-            <OrganizationTab
-              industrySubcategories={subcategories}
-              contentVersions={contentVersions}
-            />
-          </TabPanel>
-          <TabPanel value={value} tabName="teams">
-            <TeamsTab industrySubcategories={subcategories} />
-          </TabPanel>
-        </Fragment>
-      )}
-    </div>
-  )
+            </TabPanel>
+            <TabPanel value={tabValue} tabName="teams">
+              <TeamsTab industrySubcategories={industrySubcategories} />
+            </TabPanel>
+          </React.Fragment>
+        )}
+      </div>
+    )
+  }
 }
 
 CreativeDifferenceTabs.propTypes = {
