2140143c977a99b87558f6827976cb6a3926f5ff
Josh Schwartzman <josh@dreamseedo.org>
2020-05-26 09:49:46 -0700

    Refactor CollectionDateRange to functional component

--- __tests__/ui/grid/CollectionDateRange.unit.test.js
+++ __tests__/ui/grid/CollectionDateRange.unit.test.js
@@ -1,27 +1,30 @@
-import CollectionDateRange from '~/ui/grid/CollectionDateRange'
+import CollectionDateRange, {
+  DateRangeDisplay,
+} from '~/ui/grid/CollectionDateRange'
 
 import { fakeCollection } from '#/mocks/data'
 
 let props, wrapper
-const rerender = () => {
-  wrapper = shallow(<CollectionDateRange {...props} />)
-}
-beforeEach(() => {
-  props = {
-    collection: {
-      ...fakeCollection,
-      start_date: '2025-05-22T20:25:02.962Z',
-      end_date: '2025-06-26T20:25:02.962Z',
-    },
-  }
-  rerender()
-})
-
 describe('CollectionDateRange', () => {
-  it('renders dates', () => {
-    const dateRangeText = wrapper.find('.date-range').text()
-    expect(dateRangeText).toMatch('5.22.25 to 6.26.25')
-    expect(dateRangeText).not.toMatch('No dates selected')
+  const rerender = () => {
+    wrapper = shallow(<CollectionDateRange {...props} />)
+  }
+  beforeEach(() => {
+    props = {
+      collection: {
+        ...fakeCollection,
+        start_date: '2025-05-22T20:25:02.962Z',
+        end_date: '2025-06-26T20:25:02.962Z',
+      },
+    }
+    rerender()
+  })
+
+  it('renders DateRangeDisplay with date props', () => {
+    expect(wrapper.find('DateRangeDisplay').props().dateRange).toEqual([
+      props.collection.start_date,
+      props.collection.end_date,
+    ])
   })
 
   it('renders InlineModal with CollectionDateRange picker', () => {
@@ -36,19 +39,35 @@ describe('CollectionDateRange', () => {
 
   it('opens InlineModal when clicked on', () => {
     const fakeEv = { preventDefault: jest.fn() }
-    wrapper.find('.date-range').simulate('click', fakeEv)
+    wrapper.find('.date-range-wrapper').simulate('click', fakeEv)
     expect(wrapper.find('InlineModal').props().open).toEqual(true)
+  })
+})
+
+describe('DateRangeDisplay', () => {
+  const rerender = () => {
+    wrapper = shallow(<DateRangeDisplay {...props} />)
+  }
+  beforeEach(() => {
+    props = {
+      dateRange: ['2025-05-22T20:25:02.962Z', '2025-06-26T20:25:02.962Z'],
+    }
+    rerender()
+  })
+
+  it('renders dates', () => {
+    expect(wrapper.text()).toMatch('5.22.25 to 6.26.25')
+    expect(wrapper.text()).not.toMatch('No dates selected')
   })
 
   describe('without dates selected', () => {
     beforeEach(() => {
-      props.collection.start_date = null
-      props.collection.end_date = null
+      props.dateRange = [null, null]
       rerender()
     })
 
     it('renders No dates selected', () => {
-      expect(wrapper.find('.date-range').text()).toMatch('No dates selected')
+      expect(wrapper.text()).toMatch('No dates selected')
     })
   })
 })
--- app/javascript/ui/grid/CollectionDateRange.js
+++ app/javascript/ui/grid/CollectionDateRange.js
@@ -1,8 +1,9 @@
+import PropTypes from 'prop-types'
 import { PropTypes as MobxPropTypes } from 'mobx-react'
 import { StaticDateRangePicker } from '@material-ui/pickers'
 import moment from 'moment-mini'
 import styled from 'styled-components'
-import { Fragment } from 'react'
+import { Fragment, useState } from 'react'
 
 import v from '~/utils/variables'
 import TextButton from '~/ui/global/TextButton'
@@ -20,152 +21,120 @@ const EditIcon = styled.span`
   }
 `
 
-class CollectionDateRange extends React.Component {
-  constructor(props) {
-    super(props)
-    this.dateDisplayRef = React.createRef()
-    this.wrapperRef = React.createRef()
+export const DateRangeDisplay = props => {
+  const { dateRange } = props
+  if (dateRange[0] === null && dateRange[1] === null) {
+    return 'No dates selected'
+  } else {
+    return (
+      <Fragment>
+        {moment(dateRange[0]).format('M.D.YY')}
+        {' to '}
+        {moment(dateRange[1]).format('M.D.YY')}
+      </Fragment>
+    )
   }
+}
 
-  state = {
-    datePickerOpen: false,
-    dateRange: [null, null],
-    selectedDateRange: [null, null],
-  }
+DateRangeDisplay.propTypes = {
+  dateRange: PropTypes.array.isRequired,
+}
 
-  componentDidMount() {
-    const { start_date, end_date } = this.props.collection
-    if (start_date && end_date) {
-      this.setState({ dateRange: [start_date, end_date] })
-    }
-  }
+const CollectionDateRange = props => {
+  const dateDisplayRef = React.createRef()
+  const wrapperRef = React.createRef()
 
-  handleDateChange = dateRange => {
-    this.setState({
-      selectedDateRange: dateRange,
-    })
-  }
+  const { collection } = props
+  const [dateRange, setDateRange] = useState([
+    collection.start_date,
+    collection.end_date,
+  ])
+  const [datePickerOpen, setDatePickerOpen] = useState(false)
+  const [selectedDateRange, setSelectedDateRange] = useState([null, null])
+  const [previousDateRange, setPreviousDateRange] = useState(null)
 
-  handleConfirmDateChange = e => {
-    const { selectedDateRange } = this.state
-    const { collection } = this.props
-    this.setState({
-      datePickerOpen: false,
-      dateRange: selectedDateRange,
-    })
+  const handleConfirmDateChange = e => {
+    setDatePickerOpen(false)
+    setDateRange(selectedDateRange)
     collection.start_date = selectedDateRange[0]
     collection.end_date = selectedDateRange[1]
     collection.save()
   }
 
-  handleClearDates = e => {
-    const { dateRange } = this.state
+  const handleClearDates = e => {
     e.preventDefault()
     e.stopPropagation()
-    this.setState({
-      dateRange: [null, null],
-      selectedDateRange: [null, null],
-      previousDateRange: dateRange,
-    })
+    setPreviousDateRange(dateRange)
+    setDateRange([null, null])
+    setSelectedDateRange([null, null])
   }
 
-  handleCancelDateChange = e => {
-    const { previousDateRange, dateRange } = this.state
-    let setDateRange = dateRange
-
+  const handleCancelDateChange = () => {
     // If there was a previous value, and they've now canceled clearing the value,
     // restore the original value
     if (previousDateRange && dateRange[0] === null) {
-      setDateRange = previousDateRange
+      setDateRange(previousDateRange)
     }
-
-    this.setState({
-      datePickerOpen: false,
-      dateRange: setDateRange,
-    })
+    setDatePickerOpen(false)
   }
 
-  setDatePickerOpen(value) {
-    this.setState({
-      datePickerOpen: value,
-      selectedDateRange: [null, null],
-      previousDateRange: [null, null],
-    })
+  const openDatePicker = () => {
+    setDatePickerOpen(true)
+    setSelectedDateRange([null, null])
+    setPreviousDateRange([null, null])
   }
 
-  stopPropagationToParents = e => {
-    const dateRangeWrapper = this.wrapperRef.current
-    if (dateRangeWrapper && dateRangeWrapper.contains(e.target)) return
+  const stopEventPropagationToParents = ev => {
+    const dateRangeWrapper = wrapperRef.current
+    if (dateRangeWrapper && dateRangeWrapper.contains(ev.target)) return
     // Prevent any clicks from bubbling up out of the date range component
     // So that they aren't registered as collection cover clicks
-    e.stopPropagation()
+    ev.stopPropagation()
   }
 
-  get clearButton() {
-    return (
-      <TextButton
-        onClick={this.handleClearDates}
-        fontSizeEm={0.75}
-        color={v.colors.black}
-      >
-        Clear
-      </TextButton>
-    )
-  }
-
-  get dateRangeDisplay() {
-    const { dateRange } = this.state
-    if (dateRange[0] === null && dateRange[1] === null) {
-      return 'No dates selected'
-    } else {
-      return (
-        <Fragment>
-          {moment(dateRange[0]).format('M.D.YY')}
-          {' to '}
-          {moment(dateRange[1]).format('M.D.YY')}
-        </Fragment>
-      )
-    }
-  }
-
-  render() {
-    const { dateRange, datePickerOpen } = this.state
-    return (
+  return (
+    <div
+      ref={wrapperRef}
+      onClick={stopEventPropagationToParents}
+      className="cancelGridClick"
+    >
       <div
-        ref={this.collectionDateRangeWrapperRef}
-        onClick={this.stopPropagationToParents}
-        className="cancelGridClick"
+        onClick={openDatePicker}
+        ref={dateDisplayRef}
+        className="date-range-wrapper"
       >
-        <div
-          onClick={() => this.setDatePickerOpen(!datePickerOpen)}
-          ref={this.dateDisplayRef}
-          className="date-range"
-        >
-          {this.dateRangeDisplay}
-          <EditIcon>
-            <EditPencilIcon />
-          </EditIcon>
-        </div>
-        <InlineModal
-          title={'Pick Dates'}
-          onConfirm={this.handleConfirmDateChange}
-          onCancel={this.handleCancelDateChange}
-          open={datePickerOpen}
-          leftButton={this.clearButton}
-          anchorElement={this.dateDisplayRef.current}
-        >
-          <StaticDateRangePicker
-            displayStaticWrapperAs="desktop"
-            calendars={1}
-            value={dateRange}
-            onChange={this.handleDateChange}
-            DialogProps={{ fullWidth: true }}
-            renderInput={(startProps, endProps) => ''}
-          />
-        </InlineModal>
+        <DateRangeDisplay dateRange={dateRange} />
+        <EditIcon>
+          <EditPencilIcon />
+        </EditIcon>
       </div>
-    )
-  }
+      <InlineModal
+        title={'Pick Dates'}
+        onConfirm={handleConfirmDateChange}
+        onCancel={handleCancelDateChange}
+        open={datePickerOpen}
+        leftButton={
+          <TextButton
+            onClick={handleClearDates}
+            fontSizeEm={0.75}
+            color={v.colors.black}
+          >
+            Clear
+          </TextButton>
+        }
+        anchorElement={dateDisplayRef.current}
+      >
+        <StaticDateRangePicker
+          displayStaticWrapperAs="desktop"
+          calendars={1}
+          value={dateRange}
+          onChange={dateRange => setSelectedDateRange(dateRange)}
+          DialogProps={{ fullWidth: true }}
+          renderInput={(startProps, endProps) => ''}
+        />
+      </InlineModal>
+    </div>
+  )
 }
 
 CollectionDateRange.propTypes = {
