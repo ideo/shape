7a85171dd131492e9318fdebed9db3a9f34c638a
Marco Segreto <msegreto@ideo.com>
2020-06-09 16:42:45 -0700

    Linting

--- app/javascript/ui/pages/shared/PageHeader.js
+++ app/javascript/ui/pages/shared/PageHeader.js
@@ -528,7 +528,9 @@ class PageHeader extends React.Component {
                 />
               )}
             </StyledTitleAndRoles>
-            {(record.isRegularCollection || record.isSubmissionsCollection || record.isBoard) && (
+            {(record.isRegularCollection ||
+              record.isSubmissionsCollection ||
+              record.isBoard) && (
               <div style={{ marginBottom: '-16px', display: 'flex' }}>
                 <CollectionViewToggle collection={record} />
                 <CollectionFilter collection={record} canEdit={this.canEdit} />
