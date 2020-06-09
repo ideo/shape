216a8e711243ef0bad66359659e0d28501e82433
Marco Segreto <msegreto@ideo.com>
2020-06-09 12:48:22 -0700

    Replace sorting icon

--- app/javascript/ui/grid/CollectionList.js
+++ app/javascript/ui/grid/CollectionList.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Flex } from 'reflexbox'
 import { PropTypes as MobxPropTypes } from 'mobx-react'
 
-import ArrowIcon from '~/ui/icons/ArrowIcon'
+import DropdownIcon from '~/ui/icons/DropdownIcon'
 import ListCard, { Column } from './ListCard'
 import TextButton from '~/ui/global/TextButton'
 import { uiStore } from '~/stores'
@@ -64,8 +64,8 @@ class CollectionList extends React.Component {
                 {column.displayName}
               </TextButton>
               {column.sortable && (
-                <span style={{ width: '12px', height: '12px' }}>
-                  <ArrowIcon rotation={90} />
+                <span style={{ width: '20px', height: '20px' }}>
+                  <DropdownIcon />
                 </span>
               )}
             </Column>
