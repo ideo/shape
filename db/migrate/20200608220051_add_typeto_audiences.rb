ac43d1f53c3fc62c36ddf57110171121ef8c944a
niculistana <niculistana@gmail.com>
2020-06-08 19:33:08 -0700

    fix migration file audience type; remove temporary challenge audiences file

--- db/migrate/20200608220051_add_typeto_audiences.rb
+++ db/migrate/20200608220051_add_typeto_audiences.rb
@@ -3,9 +3,9 @@ class AddTypetoAudiences < ActiveRecord::Migration[5.2]
     add_column :audiences, :audience_type, :integer, null: true
 
     # if you already seeded audiences
-    Audience.find_by(name: 'Admins')&.update(audience_type: 1)
-    Audience.find_by(name: 'Reviewers')&.update(audience_type: 1)
-    Audience.find_by(name: 'Participants')&.update(audience_type: 1)
+    Audience.find_by(name: 'Admins')&.update(audience_type: 0)
+    Audience.find_by(name: 'Reviewers')&.update(audience_type: 0)
+    Audience.find_by(name: 'Participants')&.update(audience_type: 0)
   end
 
   def down
