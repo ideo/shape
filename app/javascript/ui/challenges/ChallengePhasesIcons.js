ba98654c3f8e4fdfc27a052d409c5b37d6364ddd
Josh Schwartzman <josh@dreamseedo.org>
2020-06-10 16:36:30 -0700

    Extract TooltipHeader to Typography, fix FormatDateRange import

--- app/javascript/ui/challenges/ChallengePhasesIcons.js
+++ app/javascript/ui/challenges/ChallengePhasesIcons.js
@@ -6,8 +6,8 @@ import { filter } from 'lodash'
 
 import Tooltip from '~/ui/global/Tooltip'
 import DateProgressBar from '~/ui/global/DateProgressBar'
-import { formatDateRange } from '~/ui/grid/CollectionDateRange'
-import { SmallHelperText } from '~/ui/global/styled/typography'
+import { FormatDateRange } from '~/ui/grid/CollectionDateRange'
+import { SmallHelperText, TooltipHeader } from '~/ui/global/styled/typography'
 import v from '~/utils/variables'
 
 // Temporary Icons
@@ -24,15 +24,6 @@ const PhaseIcon = styled.div`
   }
 `
 PhaseIcon.displayName = 'PhaseIcon'
-
-// I couldn't find any similar-enough style in typography.js
-// So created a new style for this tooltip
-const TooltipHeader = styled.div`
-  font-family: ${v.fonts.sans};
-  font-size: 0.9rem;
-  color: ${v.colors.white};
-  font-weight: bold;
-`
 
 export const phaseIcons = [
   <BuildIcon />,
@@ -66,7 +57,9 @@ const ChallengePhasesIcons = ({ collection }) => {
             <div style={{ textAlign: 'center' }}>
               <TooltipHeader color={v.colors.white}>{phase.name}</TooltipHeader>
               <SmallHelperText>
-                {formatDateRange([phase.start_date, phase.end_date])}
+                <FormatDateRange
+                  dateRange={[phase.start_date, phase.end_date]}
+                />
               </SmallHelperText>
             </div>
           }
