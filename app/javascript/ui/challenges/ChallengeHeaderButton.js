50df377b172cce38ededbe04b4df7d2305c3c9cf
Marco Segreto <msegreto@ideo.com>
2020-07-02 10:16:00 -0700

    Check if in current reviewers group

--- app/javascript/ui/challenges/ChallengeHeaderButton.js
+++ app/javascript/ui/challenges/ChallengeHeaderButton.js
@@ -1,5 +1,5 @@
 import _ from 'lodash'
-import { PropTypes as MobxPropTypes } from 'mobx-react'
+import { inject, observer, PropTypes as MobxPropTypes } from 'mobx-react'
 
 import { uiStore } from '~/stores'
 import Button from '~/ui/global/Button'
@@ -11,40 +11,63 @@ const buttonStyleProps = {
   style: { marginLeft: '1rem' },
 }
 
-const ChallengeHeaderButton = ({ record }) => {
-  if (!record.isChallengeOrInsideChallenge) return null
-
-  if (!record.isSubmissionBox && record.canEdit) {
-    return (
-      <Button
-        {...buttonStyleProps}
-        colorScheme={v.colors.primaryDark}
-        onClick={() => uiStore.update('challengeSettingsOpen', true)}
-      >
-        Challenge Settings
-      </Button>
-    )
-  } else {
-    // TODO: only show this if they are in the reviewer group
-    const reviewableCards = _.get(
-      record,
-      'submissions_collection.reviewableCards'
-    )
-    const hasReviewableSubmissions = !_.isEmpty(reviewableCards)
-    return (
-      <Button
-        {...buttonStyleProps}
-        colorScheme={v.colors.alert}
-        disabled={!hasReviewableSubmissions}
-        onClick={() => record.navigateToNextAvailableInCollectionTestOrTest()}
-      >
-        {hasReviewableSubmissions
-          ? `Review Submissions`
-          : `No Reviewable Submissions`}
-      </Button>
-    )
+@inject('apiStore')
+@observer
+class ChallengeHeaderButton extends React.Component {
+  componentDidMount() {
+    const { record } = this.props
+    record.fetchChallengeReviewersGroup()
   }
-  return null
+
+  // TODO combine this logic with AddReviewersPopover
+  get potentialReviewers() {
+    const { record } = this.props
+    const challengeRoles = _.get(record, 'challengeReviewerGroup.roles')
+    if (_.isEmpty(challengeRoles)) return []
+    const memberRole = challengeRoles.find(r => r.label === 'member')
+    return _.get(memberRole, 'users', [])
+  }
+
+  render() {
+    const { apiStore, record } = this.props
+
+    if (!record.isChallengeOrInsideChallenge) return null
+
+    if (!record.isSubmissionBox && record.canEdit) {
+      return (
+        <Button
+          {...buttonStyleProps}
+          colorScheme={v.colors.primaryDark}
+          onClick={() => uiStore.update('challengeSettingsOpen', true)}
+        >
+          Challenge Settings
+        </Button>
+      )
+    } else {
+      const isChallengeReviewer = this.potentialReviewers.find(
+        r => r.id === apiStore.currentUser.id
+      )
+      if (!isChallengeReviewer) return null
+      const reviewableCards = _.get(
+        record,
+        'submissions_collection.reviewableCards'
+      )
+      const hasReviewableSubmissions = !_.isEmpty(reviewableCards)
+      return (
+        <Button
+          {...buttonStyleProps}
+          colorScheme={v.colors.alert}
+          disabled={!hasReviewableSubmissions}
+          onClick={() => record.navigateToNextAvailableInCollectionTestOrTest()}
+        >
+          {hasReviewableSubmissions
+            ? `Review Submissions`
+            : `No Reviewable Submissions`}
+        </Button>
+      )
+    }
+    return null
+  }
 }
 
 ChallengeHeaderButton.propTypes = {
