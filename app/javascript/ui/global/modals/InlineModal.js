690a86ac6e74417d9be2d6a87aee32189f921af0
Marco <msegreto@miceover.com>
2020-08-04 17:15:49 -0700

    Add React reference for now for bit

--- app/javascript/ui/global/modals/InlineModal.js
+++ app/javascript/ui/global/modals/InlineModal.js
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types'
-import { Fragment } from 'react'
+import React, { Fragment } from 'react'
 import styled from 'styled-components'
 import { Popover, Grid } from '@material-ui/core'
 
