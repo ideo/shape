44b75efc2dde326bb498cf894b6b590beab1f88e
kaptron <dkaplan@ideo.com>
2020-08-05 16:43:28 -0700

    refactor user_taggable so it does not remove collection filters that are still being used

--- spec/factories/collections.rb
+++ spec/factories/collections.rb
@@ -31,7 +31,9 @@ FactoryBot.define do
       end
     end
     factory :user_profile, class: Collection::UserProfile
-    factory :submission_box, class: Collection::SubmissionBox
+    factory :submission_box, class: Collection::SubmissionBox do
+      submission_box_type :template
+    end
     factory :submissions_collection, class: Collection::SubmissionsCollection
     factory :search_collection, class: Collection::SearchCollection
 
--- spec/models/concerns/user_taggable_spec.rb
+++ spec/models/concerns/user_taggable_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe UserTaggable, type: :concern do
   let(:collection) { create(:collection) }
   let(:users) { create_list(:user, 3) }
+  let(:user) { users.first }
   subject { Collection }
 
   it 'is included in Collection' do
@@ -24,23 +25,79 @@ describe UserTaggable, type: :concern do
 
     describe '#add' do
       it 'adds tag' do
-        collection.user_tag_list.add(users[0].handle)
+        collection.user_tag_list.add(user.handle)
         expect { collection.save }.to change(UserTag, :count).by(1)
         collection.reload
-        expect(collection.user_tag_list).to eq([users[0].handle])
+        expect(collection.user_tag_list).to eq([user.handle])
       end
     end
 
     describe '#remove' do
       before do
-        collection.update(user_tag_list: [users[0].handle, users[1].handle])
+        collection.update(user_tag_list: [user.handle, users[1].handle])
       end
 
       it 'removes tag' do
-        collection.user_tag_list.remove(users[0].handle)
+        collection.user_tag_list.remove(user.handle)
         expect { collection.save }.to change(UserTag, :count).by(-1)
         collection.reload
         expect(collection.user_tag_list).to eq([users[1].handle])
+      end
+    end
+
+    context 'with a challenge submission' do
+      let(:challenge) { create(:collection, collection_type: :challenge) }
+      let(:submission_box) { create(:submission_box, parent_collection: challenge) }
+      let(:submissions_collection) { submission_box.submissions_collection }
+      let(:submission) { create(:collection, :submission, parent_collection: submissions_collection) }
+      # the submission is the collection being tagged
+      let(:collection) { submission }
+
+      before { submission_box.setup_submissions_collection! }
+
+      describe '#add' do
+        it 'creates a reviewer filter on the submissions collection' do
+          collection.user_tag_list.add(user.handle)
+          expect {
+            collection.save
+          }.to change(submissions_collection.collection_filters, :count).by(1)
+          user_filter = submissions_collection.collection_filters.first.user_collection_filters.first
+          expect(user_filter.user).to eq user
+          expect(user_filter.selected).to be true
+        end
+      end
+
+      describe '#remove' do
+        before do
+          collection.update(user_tag_list: [user.handle])
+        end
+
+        it 'removes the reviewer filter on the submissions collection' do
+          expect(submissions_collection.collection_filters.count).to eq 1
+          # now remove
+          expect {
+            collection.update(user_tag_list: [])
+          }.to change(submissions_collection.collection_filters, :count).by(-1)
+          expect(submissions_collection.collection_filters.count).to eq 0
+        end
+
+        context 'when reviewer still relevant via another submission' do
+          let(:submission_2) { create(:collection, :submission, parent_collection: submissions_collection) }
+          before do
+            # both submissions are now tagged with the user
+            submission.update(user_tag_list: [user.handle])
+            submission_2.update(user_tag_list: [user.handle])
+          end
+
+          it 'keeps the reviewer filter on the submissions collection' do
+            expect(submissions_collection.collection_filters.count).to eq 1
+            # now remove -- count should stay the same
+            expect {
+              submission_2.update(user_tag_list: [])
+            }.not_to change(submissions_collection.collection_filters, :count)
+            expect(submissions_collection.collection_filters.count).to eq 1
+          end
+        end
       end
     end
   end
