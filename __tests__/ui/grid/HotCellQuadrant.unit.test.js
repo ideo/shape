59316aa9a02dfc80f5f61d2b70a610f385130491
Marco <msegreto@miceover.com>
2020-10-20 13:10:33 -0700

    Fix test

--- __tests__/ui/grid/HotCellQuadrant.unit.test.js
+++ __tests__/ui/grid/HotCellQuadrant.unit.test.js
@@ -1,7 +1,7 @@
 import HotCellQuadrant, { Quadrant } from '~/ui/grid/HotCellQuadrant'
 import LinkIcon from '~/ui/icons/htc/LinkIcon'
+import PlusIcon from '~/ui/icons/PlusIcon'
 import PopoutMenu from '~/ui/global/PopoutMenu'
-import TemplateIcon from '~/ui/icons/htc/TemplateIcon'
 import TextIcon from '~/ui/icons/htc/TextIcon'
 
 import fakeUiStore from '#/mocks/fakeUiStore'
@@ -75,8 +75,8 @@ describe('HotCellQuadrant', () => {
         rerender({ name: 'template' })
       })
 
-      it('should render te TemplateIcon', () => {
-        expect(wrapper.find(TemplateIcon).exists()).toBe(true)
+      it('should render the PlusIcon', () => {
+        expect(wrapper.find(PlusIcon).exists()).toBe(true)
       })
     })
   })
